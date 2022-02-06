102638f91c36d440e7150df60e2a071ea0c01b56
khadeja Sabry Sayed <k.Sayed@fintech.local>
2022-02-06 17:04:18 +0200

    refactoring:delete unused things

--- app/Models/AppMedia.php
+++ app/Models/AppMedia.php
@@ -4,12 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Traits\Uuid;
 
 class AppMedia extends Model
 {
-    use HasFactory;
+    use HasFactory, Uuid;
+
+    #region properties
+    #endregion properties
+
+    #region mutators
+    #endregion mutators
+
+    #region scopes
+    #endregion scopes
+
+    #region relationships
+    #endregion relationships
+
+    #region custom Methods
+    #endregion custom Methods
+
+
     protected $guarded = ['created_at','updated_at','deleted_at'];
-
-
-    
 }
