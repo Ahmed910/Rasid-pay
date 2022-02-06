ff98b75763899be59581aee40fe7eaeed1bcaba9
Mohamed Fathi <mohamed.metwally@rasid.com.sa>
2022-02-06 11:04:16 +0200

    add permissions model and table

---
+++ app/Models/Permission.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\Uuid;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Permission extends Model
+{
+    use HasFactory, Uuid;
+
+    #region properties
+    protected $guarded = ["created_at", "updated_at"];
+    #endregion properties
+
+    #region mutators
+    #endregion mutators
+
+    #region scopes
+    #endregion scopes
+
+    #region relationships
+    #endregion relationships
+
+    #region custom Methods
+    #endregion custom Methods
+}
