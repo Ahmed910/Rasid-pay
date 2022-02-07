5045110edf71f9d4f53323bd4ac3368a72c493ff
khadeja Sabry Sayed <k.Sayed@fintech.local>
2022-02-07 11:56:47 +0200

    adding some changes

---
+++ app/Http/Resources/Dashboard/RoleResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources\Dashboard;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class RoleResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+       // return parent::toArray($request);
+
+
+
+       return [
+'name'=>$this->name,
+'user_name'=>$this->username
+
+
+       ];
+
+    }
+}
