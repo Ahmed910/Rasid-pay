5ce8c7141a41074a38f5292e145e86b2455f83ba
Mohamed Zaky <m.zaky@fintech.local>
2022-02-07 11:50:15 +0200

    change migration models relationships

--- app/Models/City/CityTranslation.php
+++ app/Models/City/CityTranslation.php
@@ -10,22 +10,6 @@ class CityTranslation extends Model
 {
     use HasFactory, Uuid;
 
-    #region properties
     protected $guarded = ['created_at', 'updated_at', 'deleted_at'];
-    #endregion properties
-
-    #region mutators
-    #endregion mutators
-
-    #region scopes
-    #endregion scopes
-
-    #region relationships
-    #endregion relationships
-
-    #region custom Methods
-    #endregion custom Methods
     public $timestamps = false;
-
-
 }
--- app/Models/Currency/Currency.php
+++ app/Models/Currency/Currency.php
@@ -6,12 +6,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Traits\Uuid;
 
-class AppMedia extends Model
+class Currency extends Model
 {
     use HasFactory, Uuid;
 
     #region properties
-    protected $guarded = ['created_at', 'updated_at', 'deleted_at'];
     #endregion properties
 
     #region mutators
@@ -21,14 +20,8 @@ class AppMedia extends Model
     #endregion scopes
 
     #region relationships
-    public function mediable()
-    {
-        return $this->morphTo();
-    }
     #endregion relationships
 
     #region custom Methods
     #endregion custom Methods
-
-
 }
