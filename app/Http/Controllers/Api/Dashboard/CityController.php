682eef58f3be8887ac546929ce9c71ca05c227f7
Nader Hany <n.hany@fintech.local>
2022-02-07 11:55:04 +0200

    edit regions table

---
+++ app/Http/Controllers/Api/Dashboard/CityController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers\Api\Dashboard;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Dashboard\Cities\StoreCityRequest;
+use App\Models\City\City;
+use Illuminate\Http\Request;
+
+class CityController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreCityRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\City  $city
+     * @return \Illuminate\Http\Response
+     */
+    public function show(City $city)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\City  $city
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, City $city)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\City  $city
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(City $city)
+    {
+        //
+    }
+}
