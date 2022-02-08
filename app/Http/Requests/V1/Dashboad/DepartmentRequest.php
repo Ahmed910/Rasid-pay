7b5ca5e5da8e49300b6b6460b36423c6869873d7
Mohamed Fathi <mohamed.metwally@rasid.com.sa>
2022-02-08 09:42:13 +0200

    refactor: when departmentFormRequest updated

--- app/Http/Requests/V1/Dashboad/DepartmentRequest.php
+++ app/Http/Requests/V1/Dashboad/DepartmentRequest.php
@@ -29,9 +29,9 @@ class DepartmentRequest extends ApiMasterRequest
 
         ];
         foreach (config('translatable.locales') as $locale) {
-            $rules[$locale]               = "array";
-            $rules["$locale.name"]        = "required|max:255|string|unique:department_translations,name," . $this->id;
-            $rules["$locale.description"] = "required|string";
+            $rules["$locale"]               = "array";
+            $rules["$locale.name"]          = "required|max:255|string|unique:department_translations,name," . ($this->id ?? 0);
+            $rules["$locale.description"]   = "required|string";
         }
 
         return $rules;
