fb44125f24d2b147b9bc0dec6b25e1bddb52e480
taha mohamed <t.mohamed@fintech.local>
2022-02-13 10:52:58 +0200

    Add Devices

--- app/Http/Middleware/UserLocale.php
+++ app/Http/Middleware/UserLocale.php
@@ -15,9 +15,9 @@ class UserLocale
     public function handle($request, Closure $next)
     {
         $locale = $request->header('Accept-Language');
-        if (auth()->check() && auth()->user()->user_locale != $locale) {
+        if (auth()->check() && $locale && auth()->user()->user_locale != $locale) {
              app()->setLocale($locale);
-             auth('api')->user()->update(['user_locale' => $locale]);
+             auth()->user()->update(['user_locale' => $locale]);
         }elseif($locale != app()->getLocale() && in_array($locale,config('translatable.locales'))){
             app()->setLocale($locale);
         }
--- routes/v1/dashboard.php
+++ routes/v1/dashboard.php
@@ -15,46 +15,41 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
-
 Route::post('login', "AuthController@login");
+Route::middleware('auth:sanctum','adminPermission')->group(function(){
+
+    Route::controller('CountryController')->name('countries.')->prefix('countries')->group(function () {
+        Route::get('archive', 'archive')->name('archive');
+        Route::post('restore/{id}', 'restore')->name('restore');
+        Route::delete('forceDelete/{id}', 'forceDelete')->name('forceDelete');
+    });
+
+    Route::controller('CurrencyController')->prefix('currencies')->group(function () {
+        Route::get('archive', 'archive')->name('archive');
+        Route::post('restore/{id}', 'restore')->name('restore');
+        Route::delete('forceDelete/{id}', 'forceDelete')->name('forceDelete');
+    });
+
+    Route::controller('CityController')->name('cities.')->prefix('cities')->group(function () {
+        Route::get('archive', 'archive')->name('archive');
+        Route::post('restore/{id}', 'restore')->name('restore');
+        Route::delete('forceDelete/{id}', 'forceDelete')->name('forceDelete');
+    });
 
 
+    Route::controller('RegionController')->name('regions.')->prefix('regions')->group(function () {
+        Route::get('archive/get', 'archive')->name('archive');
+        Route::post('restore/{id}', 'restore')->name('restore');
+        Route::delete('forceDelete/{id}', 'forceDelete')->name('forceDelete');
+    });
 
+    Route::resources([
+        'countries' => 'CountryController',
+        'currencies' => 'CurrencyController',
+        "departments" => "DepartmentController",
+        "cities" => "CityController",
+        "regions" => "RegionController",
+        'roles' => 'RoleController',
+    ]);
 
-
-Route::controller('CountryController')->name('countries.')->prefix('countries')->group(function () {
-    Route::get('archive', 'archive')->name('archive');
-    Route::post('restore/{id}', 'restore')->name('restore');
-    Route::delete('forceDelete/{id}', 'forceDelete')->name('forceDelete');
 });
-
-Route::controller('CurrencyController')->prefix('currencies')->group(function () {
-    Route::get('archive', 'archive')->name('archive');
-    Route::post('restore/{id}', 'restore')->name('restore');
-    Route::delete('forceDelete/{id}', 'forceDelete')->name('forceDelete');
-});
-
-Route::controller('CityController')->name('cities.')->prefix('cities')->group(function () {
-    Route::get('archive', 'archive')->name('archive');
-    Route::post('restore/{id}', 'restore')->name('restore');
-    Route::delete('forceDelete/{id}', 'forceDelete')->name('forceDelete');
-});
-
-
-Route::controller('RegionController')->name('regions.')->prefix('regions')->group(function () {
-    Route::get('archive/get', 'archive')->name('archive');
-    Route::post('restore/{id}', 'restore')->name('restore');
-    Route::delete('forceDelete/{id}', 'forceDelete')->name('forceDelete');
-});
-
-Route::resources([
-    'countries' => 'CountryController',
-    'currencies' => 'CurrencyController',
-    "departments" => "DepartmentController",
-    "cities" => "CityController",
-    "regions" => "RegionController",
-    'roles' => 'RoleController',
-]);
