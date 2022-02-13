f4c19ba3d3393449202ed10f3122f085c76dcb63
Nader Hany <php-nader@fintech-sa.com>
2022-02-13 15:04:41 +0200

    add rasid_jobs models and migration

---
+++ app/Models/RasidJob/RasidJob.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models\RasidJob;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Traits\Uuid;
+use Astrotomic\Translatable\Contracts\Translatable as TranslatableContract;
+use Astrotomic\Translatable\Translatable;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class RasidJob extends Model implements TranslatableContract
+{
+    use HasFactory, Uuid, Translatable, SoftDeletes;
+
+    #region properties
+    const VACANT   = "vacant";
+    const OCCUPIED = "occupied";
+    const TYPES    = [self::VACANT, self::OCCUPIED];
+
+    protected $guarded = ['created_at', 'updated_at', 'deleted_at'];
+    public $translatedAttributes = ['name', 'description'];
+    #endregion properties
+
+    #region mutators
+    #endregion mutators
+
+    #region scopes
+    #endregion scopes
+
+    #region relationships
+    #endregion relationships
+
+    #region custom Methods
+    #endregion custom Methods
+}
