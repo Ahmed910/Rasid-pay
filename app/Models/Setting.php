140679d9aaee811995b718540559a337114ee02a
Mohamed Fathi <mohamed.metwally@rasid.com.sa>
2022-02-17 10:31:45 +0200

    update settings (migrations,contrller , requst , ...)

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -9,5 +9,27 @@ use App\Traits\Uuid;
 class Setting extends Model
 {
     use HasFactory, Uuid;
-    protected $guarded = ['created_at','updated_at','deleted_at'];
+
+    protected $guarded = ['created_at', 'updated_at', 'deleted_at'];
+    protected $casts   = ['value' => 'array'];
+
+    const TEXT_AREA = "textarea";
+    const EDITOR    = "editor";
+    const TEXT      = "text";
+    const FILE      = "file";
+    const NUMBER    = "number";
+
+    const TYPES = [
+        self::TEXT_AREA,
+        self::EDITOR,
+        self::TEXT,
+        self::FILE,
+        self::NUMBER
+    ];
+
+    const ERP = "erp";
+
+    const DASHBOARD_TYPES = [
+        self::ERP
+    ];
 }
