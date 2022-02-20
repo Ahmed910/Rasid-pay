c774ce0e62d3eddb53150278903f90eef5b5dbb6
Nader Hany <php-nader@fintech-sa.com>
2022-02-20 17:02:12 +0200

    add default null when transalations description is not found

--- app/Http/Resources/Dashboard/GlobalTransResource.php
+++ app/Http/Resources/Dashboard/GlobalTransResource.php
@@ -18,9 +18,9 @@ class GlobalTransResource extends JsonResource
             'id' => $this->id,
             'locale' => $this->locale,
             'name' => $this->name,
-            'desc' => $this->when($this->desc,$this->desc),
-            'description' => $this->when($this->description,$this->description),
-            'nationality' => $this->when($this->nationality,$this->nationality),
+            'desc' => $this->when($this->desc,$this->desc,null),
+            'description' => $this->when($this->description,$this->description,null),
+            'nationality' => $this->when($this->nationality,$this->nationality,null),
         ];
     }
 }
