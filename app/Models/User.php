0b22d13a34e02a00cdfc238dc2873344b140e47b
Mohamed Zaky <m.zaky@fintech.local>
2022-02-22 15:02:48 +0200

    edit admin controller

--- app/Models/User.php
+++ app/Models/User.php
@@ -126,7 +126,6 @@ class User extends Authenticatable implements HasAssetsInterface
             $this->attributes['is_ban_always'] = null;
             $this->attributes['ban_from'] = null;
             $this->attributes['ban_to'] = null;
-            $this->attributes['is_ban'] = null;
         }
     }
 
@@ -169,6 +168,4 @@ class User extends Authenticatable implements HasAssetsInterface
             $query->where('is_active', $request->is_active);
         }
     }
-
-
 }
