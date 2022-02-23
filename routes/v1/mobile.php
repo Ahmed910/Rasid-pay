0e4c2213164278dbf16458f60d671070ab8e1af1
Mohamed Fathi <mohamed.metwally@rasid.com.sa>
2022-02-23 15:33:53 +0200

    Add Mobile ContactController , Request and Route

--- routes/v1/mobile.php
+++ routes/v1/mobile.php
@@ -1,8 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\Dashboard\v1\CountryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,6 +13,4 @@ use App\Http\Controllers\Api\Dashboard\v1\CountryController;
 |
 */
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
+Route::post('send-message', 'ContactController@sendMessage')->name('send_message');
