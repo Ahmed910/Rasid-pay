cde1690eb2caf60186f6a487527142c38b031d36
Taha <taha.mohamed2016@gmail.com>
2022-02-23 12:28:43 +0200

    Edit Trans

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -66,5 +66,6 @@ class Kernel extends HttpKernel
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'setLocale'   => \App\Http\Middleware\UserLocale::class,
         'adminPermission'   => \App\Http\Middleware\AdminMiddleware::class,
+        'clientCheck'   => \App\Http\Middleware\ClientMiddleware::class,
     ];
 }
