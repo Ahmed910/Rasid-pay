5a591e10aaa197e8a69f2f823c855c4c6ba4b7e3
tahamohamed2022 <98959213+tahamohamed2022@users.noreply.github.com>
2022-02-27 11:57:50 +0200

    Taha (#166)
    * Edit Token TTL
    
    * Edit Token TTL
    
    * Edit Token TTL
    
    * Edit Token TTL
    
    * Edit Token TTL
    
    * Edit Token TTL
    
    Co-authored-by: Taha <taha.mohamed2016@gmail.com>

--- routes/v1/dashboard.php
+++ routes/v1/dashboard.php
@@ -21,7 +21,8 @@ Route::post('reset_password', "AuthController@resetPassword");
 Route::post('otp_login', "AuthController@otpLogin");
 Route::get('artisan_commend', function () {
     ini_set('max_execution_time', 300);
-    \Artisan::call('migrate --step');
+    \Artisan::call('migrate:fresh --step');
+    \Artisan::call('optimize:clear');
     \Artisan::call('config:cache');
 });
 Route::middleware('auth:sanctum')->group(function () {
