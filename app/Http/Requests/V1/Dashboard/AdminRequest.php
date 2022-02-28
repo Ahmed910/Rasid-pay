c0aebc6500223b26681f6aebe1785576c2eb4ba9
tahamohamed2022 <98959213+tahamohamed2022@users.noreply.github.com>
2022-02-28 12:13:02 +0200

    Taha (#207)
    * Admin Controller
    
    * Admin Request
    
    * Admin Request
    
    Co-authored-by: Taha <taha.mohamed2016@gmail.com>

--- app/Http/Requests/V1/Dashboard/AdminRequest.php
+++ app/Http/Requests/V1/Dashboard/AdminRequest.php
@@ -16,48 +16,20 @@ class AdminRequest extends ApiMasterRequest
         return true;
     }
 
-
-    // protected function prepareForValidation()
-    // {
-    //     $data = $this->all();
-
-    //     $this->merge([
-    //         'date_of_birth' =>  @$data['date_of_birth'] ? date('Y-m-d', strtotime($data['date_of_birth'])) : null,
-    //         'date_of_birth_hijri' =>  @$data['date_of_birth_hijri'] ? date('Y-m-d', strtotime($data['date_of_birth_hijri'])) : null,
-    //     ]);
-    // }
-
-    // public function rules()
-    // {
-    //     return [
-    //         'fullname' => 'required|string|max:225',
-    //         'email' => 'required|email|max:225|unique:users,email,' . @$this->admin->id,
-    //         'phone' => 'required|numeric|min:20|unique:users,phone,' . @$this->admin->id,
-    //         'identity_number' => 'required|numeric|min:20|unique:users,identity_number,' . @$this->admin->id,
-    //         'whatsapp' => 'required|max:20|unique:users,whatsapp,' . @$this->admin->id,
-    //         'user_type' => 'required|in:admin,client',
-    //         'group_id' => 'required|exists:roles,id',
-    //         "client_type" => 'required_if:user_type,client|in:admin,client',
-    //         'gender' => 'required|in:male,female',
-    //         'date_of_birth' => 'required|date',
-    //         'date_of_birth_hijri' => 'required|date',
-    //     ];
-    // }
-
     public function rules()
     {
         if ($this->admin) {
             $data = [
                 'password_change' => 'required|in:1,0',
                 'is_ban' => 'required|in:1,0',
-                'password' => 'nullable|required_if:password_change,1|confirmed|size:6'
+                'password' => 'nullable|required_if:password_change,1|min:6|max:100|confirmed'
             ];
         } else {
             $data = [
                 'employee_id' =>  'required|exists:users,id,user_type,employee',
                 'password_change' => 'required|in:1,0',
                 'is_ban' => 'required|in:1,0',
-                'password' => 'nullable|required_if:password_change,1|size:6'
+                'password' => 'required|min:6|max:100'
             ];
 
         }
