d12e9a1d8333bc8da1975d0b3be4b793b3addde0
Taha <taha.mohamed2016@gmail.com>
2022-03-01 14:04:14 +0200

    Edit Group Controller

--- app/Models/Group/GroupTranslation.php
+++ app/Models/Group/GroupTranslation.php
@@ -2,13 +2,13 @@
 
 namespace App\Models\Group;
 
-use App\Traits\Uuid;
+use App\Traits\{Loggable, Uuid};
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class GroupTranslation extends Model
 {
-    use HasFactory, Uuid;
+    use HasFactory, Uuid , Loggable;
 
     #region properties
     public $timestamps = false;
