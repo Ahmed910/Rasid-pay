a8b805a1904179f9dab93e1c415205878dfde2ca
eman rashad <emanrashad173.fintech@gmail.com>
2022-03-03 15:11:51 +0200

    add activaty log index

--- app/Http/Controllers/Api/V1/Dashboard/ActivityController.php
+++ app/Http/Controllers/Api/V1/Dashboard/ActivityController.php
@@ -1,16 +1,27 @@
 <?php
 
 namespace App\Http\Controllers\Api\V1\Dashboard;
+use App\Models\ActivityLog;
+use App\Http\Resources\Dashboard\ActivityLogResource;
+
+
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class ActivityController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-         
+        $activatyLogs = ActivityLog::latest()->paginate((int)($request->per_page ?? 15));
+
+        return ActivityLogResource::collection($activatyLogs)
+            ->additional([
+                'status' => true,
+                'message' => ''
+            ]);
     }
+
     public function show()
     {
 
