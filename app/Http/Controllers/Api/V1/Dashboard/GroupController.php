3e18b8ff9dfeb787fdf0c663c9123b377efeab1b
Taha <taha.mohamed2016@gmail.com>
2022-03-08 11:51:20 +0200

    Edit Group Update

--- app/Http/Controllers/Api/V1/Dashboard/GroupController.php
+++ app/Http/Controllers/Api/V1/Dashboard/GroupController.php
@@ -66,7 +66,7 @@ class GroupController extends Controller
      */
     public function update(GroupRequest $request, Group $group)
     {
-        $group->update(array_only($request->validated(),config('translatable.locales')+['is_active']));
+        $group->fill($request->validated())->save();
         $group->permissions()->sync($request->permission_list);
         return GroupResource::make($group)->additional(['status' => true, 'message' => trans('dashboard.general.success_update')]);
     }
