6d2186c4830b557becf739ed0b281b8f282f8f55
Taha <taha.mohamed2016@gmail.com>
2022-03-09 09:19:42 +0200

    Edit Rasid Job Request

--- app/Http/Requests/V1/Dashboard/RasidJobRequest.php
+++ app/Http/Requests/V1/Dashboard/RasidJobRequest.php
@@ -25,7 +25,7 @@ class RasidJobRequest extends ApiMasterRequest
     public function rules()
     {
         $rules = [
-            "department_id" => "required|exists:departments,id,deleted_at,NULL",
+            "department_id" => "required|exists:departments,id,deleted_at,NULL,is_active,1",
             "is_active" => "boolean",
             "is_vacant" => "boolean",
         ];
