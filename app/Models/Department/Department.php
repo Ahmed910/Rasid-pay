2e410b54d30eaf7c711534a1a4103115e7dbc58a
Taha <taha.mohamed2016@gmail.com>
2022-03-16 10:08:05 +0200

    Sort Departments

--- app/Models/Department/Department.php
+++ app/Models/Department/Department.php
@@ -88,7 +88,7 @@ class Department extends Model implements TranslatableContract, HasAssetsInterfa
 
             return $query->orderBy($request->sort["column"], @$request->sort["dir"]);
         }
-        return $query->latest();
+        return $query->orderBy('created_at');
     }
     #endregion scopes
 
