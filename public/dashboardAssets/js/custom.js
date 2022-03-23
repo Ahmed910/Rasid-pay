edea5807814d1c00e8a9b3ae3f19c800345dcc38
Mohamed Zaki <php-zaki@fintech-sa.com>
2022-03-23 10:00:21 +0200

    remove global image style

--- public/dashboardAssets/js/custom.js
+++ public/dashboardAssets/js/custom.js
@@ -419,20 +419,20 @@ $(document).ready(function () {
 
   $("input").attr("autocomplete", "off");
 
-  $(".dropify").dropify({
-    messages: {
-      default: "اسحب وأسقط او قم برفع الصورة",
-      replace: "اسحب وأسقط او إضغط لتغيير الصورة",
-      remove: "حذف",
-      error: "اووه ، حدث خطأ ما",
-    },
-    error: {
-      fileSize: "حجم الملف كبير (5M max).",
-      minWidth: "The image width is too small ({{ value }}}px min).",
-      maxWidth: "The image width is too big ({{ value }}}px max).",
-      minHeight: "The image height is too small ({{ value }}}px min).",
-      maxHeight: "The image height is too big ({{ value }}px max).",
-      imageFormat: "The image format is not allowed ({{ value }} only).",
-    },
-  });
+//   $(".dropify").dropify({
+//     messages: {
+//       default: "اسحب وأسقط او قم برفع الصورة",
+//       replace: "اسحب وأسقط او إضغط لتغيير الصورة",
+//       remove: "حذف",
+//       error: "اووه ، حدث خطأ ما",
+//     },
+//     error: {
+//       fileSize: "حجم الملف كبير (5M max).",
+//       minWidth: "The image width is too small ({{ value }}}px min).",
+//       maxWidth: "The image width is too big ({{ value }}}px max).",
+//       minHeight: "The image height is too small ({{ value }}}px min).",
+//       maxHeight: "The image height is too big ({{ value }}px max).",
+//       imageFormat: "The image format is not allowed ({{ value }} only).",
+//     },
+//   });
 });
