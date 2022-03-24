eacbc197724b9e82cb967e7490dc1a064f1fb38a
Mohamed Zaki <php-zaki@fintech-sa.com>
2022-03-24 10:04:12 +0200

    add translation to departments

--- app/Http/Resources/Blade/Dashboard/Job/JobResource.php
+++ app/Http/Resources/Blade/Dashboard/Job/JobResource.php
@@ -21,6 +21,7 @@ class JobResource extends JsonResource
             'department_name' => optional($this->department)->name,
             'created_at' => $this->created_at,
             'is_active' => $this->is_active,
+            'is_vacant' => $this->is_vacant,
             'show_route' => route('dashboard.job.show', $this->id),
             'edit_route' => route('dashboard.job.edit', $this->id),
             'delete_route' => route('dashboard.job.destroy', $this->id)
