2743e22134f3366a512f86525f263af58ed90020
eman rashad <emanrashad173.fintech@gmail.com>
2022-04-04 14:50:25 +0200

    edit in reset form

--- resources/views/dashboard/auth/reset.blade.php
+++ resources/views/dashboard/auth/reset.blade.php
@@ -37,14 +37,13 @@
                     <div class="form-group">
                       <label for="email">البريد الإلكتروني</label>
                       <input  onselectstart="return false" onpaste="return false" onCopy="return false" onCut="return false" onDrag="return false" onDrop="return false" autocomplete=off
-                        type="email"
+                        type="text"
                         class="form-control @error('email') is-invalid @enderror"
                         id="email"
                         name="email"
                         value="{{ old('email') }}"
                         placeholder="البريد الإلكتروني"
-                        pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
-                        required
+                        
                       />
                       @error('email')
                           <div class="invalid-feedback">{{ $message }}</div>
