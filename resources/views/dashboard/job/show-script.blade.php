e848907d4b5215812d0f1335584746cab8e192c1
Mohamed Karem <karem.fintech@gmail.com>
2022-04-05 11:53:41 +0200

    change datatable info to arabic

--- resources/views/dashboard/job/show-script.blade.php
+++ resources/views/dashboard/job/show-script.blade.php
@@ -81,8 +81,8 @@
                 ],
                 "language": {
                     "lengthMenu": "@lang('dashboard.general.show') _MENU_",
-                    "emptyTable": "@lang('dashboard.general.no_data')",
-                    "info": "@lang('dashboard.general.showing') _START_ @lang('dashboard.general.to') _END_ @lang('dashboard.general.from') _TOTAL_ @lang('dashboard.general.entries')",
+                    "emptyTable": "@lang('dashboard.datatable.no_data')",
+                    "info": "@lang('dashboard.datatable.showing') _START_ @lang('dashboard.datatable.to') _END_ @lang('dashboard.datatable.from') _TOTAL_ @lang('dashboard.datatable.entries')",
                     "infoEmpty": "",
                     "paginate": {
                         "next": '<i class="mdi mdi-chevron-left"></i>',
