cd8fcbc0824a4baf9d1609d9f5191cb615de482f
Mohamed Karem <karem.fintech@gmail.com>
2022-04-11 11:28:14 +0200

    translate titles

--- resources/lang/en/dashboard.php
+++ resources/lang/en/dashboard.php
@@ -87,6 +87,7 @@ return [
         'export' => 'Export',
         'report' => 'Print Report',
         'details' => 'Details',
+        'email' => 'Email',
         'no_data' => 'No Data',
         'reason' =>'Reason',
         'done_by' => 'Done By',
