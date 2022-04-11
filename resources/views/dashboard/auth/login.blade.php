4e83b004ea9bd69a6303bee9664811fbe6d46a58
Mohamed Zaki <php-zaki@fintech-sa.com>
2022-04-11 14:29:49 +0200

    ban user

--- resources/views/dashboard/auth/login.blade.php
+++ resources/views/dashboard/auth/login.blade.php
@@ -12,8 +12,7 @@
   <div class="form-group">
     <label for="userID">{{ trans('auth.userID')}}</label>
     <input oninput="javascript: if (this.value.length > this.maxLength) this.value = this.value.slice(0, this.maxLength);" type="number" pattern="^[1-9]\d*$" maxlength="6" class="form-control stop-copy-paste number-regex @error('username') is-invalid @enderror"
-           id="userID"
-           name="username"
+           id="userId" name="username"
     value="{{ old('username') }}"
     placeholder="{{ trans('auth.userID')}}"
     />
@@ -29,8 +28,7 @@
     <div class="input-group" id="show_hide_password">
       <input class="form-control stop-copy-paste @error('password') is-invalid @enderror"
       placeholder="{{ trans('auth.password')}}"
-      type="password"
-             name="password"
+      type="password"  name="password"
 
       />
 
