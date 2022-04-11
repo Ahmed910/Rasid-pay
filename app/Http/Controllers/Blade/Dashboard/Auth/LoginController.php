b78e673c61a9973c7b2fdb4558ca2b3ed8e23618
Taha <taha.mohamed2016@gmail.com>
2022-04-11 14:17:53 +0200

    Fix Conflict

--- app/Http/Controllers/Blade/Dashboard/Auth/LoginController.php
+++ app/Http/Controllers/Blade/Dashboard/Auth/LoginController.php
@@ -98,44 +98,6 @@ class LoginController extends Controller
         return 'login_id';
     }
 
-    public function login(LoginRequest $request)
-    {
-<<<<<<< HEAD
-        if (!$request->ajax()) {
-            // If the class is using the ThrottlesLogins trait, we can automatically throttle
-            // the login attempts for this application. We'll key this by the username and
-            // the IP address of the client making these requests into this application.
-            if (method_exists($this, 'hasTooManyLoginAttempts') &&
-            $this->hasTooManyLoginAttempts($request)) {
-                $this->fireLockoutEvent($request);
-
-                return $this->sendLockoutResponse($request);
-            }
-
-            if ($this->attemptLogin($request)) {
-                if ($request->hasSession()) {
-                    $request->session()->put('auth.password_confirmed_at', time());
-                }
-
-                return $this->sendLoginResponse($request);
-            }
-
-            // If the login attempt was unsuccessful we will increment the number of attempts
-            // to login and redirect the user back to the login form. Of course, when this
-            // user surpasses their maximum number of attempts they will get locked out.
-            $this->incrementLoginAttempts($request);
-
-            return $this->sendFailedLoginResponse($request);
-        }
-
-=======
-        $username = $this->username() == 'login_id' ? ['username' => 'required|numeric'] : ['username' => 'required|email'];
-        $request->validate([
-            'password' => 'required|string'
-        ] + $username);
->>>>>>> 86c099fca8d9dfa77355d7b73caafb02550ceef0
-    }
-
     // protected function validateLogin(LoginRequest $request)
     // {
     //    $username = $this->username() == 'login_id' ? ['username' => 'required|numeric'] : ['username' => 'required|email'];
