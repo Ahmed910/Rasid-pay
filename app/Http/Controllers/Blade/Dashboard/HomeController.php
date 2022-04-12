4a1e137d9d13eefa8e98a2f0ec8a26081e4bf831
Khadija Sabry <php-kdija@fintech-sa.com>
2022-04-12 12:02:34 +0200

    Add Home Cards

--- app/Http/Controllers/Blade/Dashboard/HomeController.php
+++ app/Http/Controllers/Blade/Dashboard/HomeController.php
@@ -3,13 +3,26 @@
 namespace App\Http\Controllers\Blade\Dashboard;
 
 use App\Http\Controllers\Controller;
+use App\Models\Department\Department;
+use App\Models\Employee;
+use App\Models\RasidJob\RasidJob;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index(Request $request)
     {
-        return view('dashboard.home.index');
+
+$departments=Department::count();
+$employees=Employee::count();
+$Active_users=User::where('ban_status','active')->where('user_type', 'admin')->count();
+$permanent_users=User::where('ban_status','permanent')->count();
+$temporary_users=User::where('ban_status','temporary')->count();
+$vacant_jobs=RasidJob::where('is_vacant',1)->count();
+$unvacant_jobs=RasidJob::where('is_vacant',0)->count();
+
+        return view('dashboard.home.index',compact('departments','employees','Active_users','temporary_users','permanent_users','vacant_jobs','unvacant_jobs'));
     }
 
     public function backButton()
