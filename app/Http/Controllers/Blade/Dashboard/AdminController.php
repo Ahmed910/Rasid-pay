9007b1d0b2b149ad7f7c0db0c1a6bcf843364c82
Taha <taha.mohamed2016@gmail.com>
2022-04-13 12:12:49 +0200

    Add Disabled Auth Buttons

--- app/Http/Controllers/Blade/Dashboard/AdminController.php
+++ app/Http/Controllers/Blade/Dashboard/AdminController.php
@@ -131,7 +131,10 @@ class AdminController extends Controller
         $previousUrl = url()->previous();
         (strpos($previousUrl, 'admin')) ? session(['perviousPage' => 'admin']) : session(['perviousPage' => 'home']);
         $admin = User::where('user_type', 'admin')->findOrFail($id);
-        return view('dashboard.admin.edit',compact('admin'));
+        $departments = Department::with('parent.translations')->ListsTranslations('name')->pluck('name', 'id')->toArray();
+        $groups = Group::ListsTranslations('name')->pluck('name', 'id');
+        $locales = config('translatable.locales');
+        return view('dashboard.admin.edit',compact('admin','departments','groups','locales'));
     }
 
     /**
