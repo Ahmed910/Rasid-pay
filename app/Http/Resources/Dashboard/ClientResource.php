752128b0148229bdbce61b8c568821dfd810619b
FINTECH\m.hussien <mohamedhussien.fintech@gmail.com>
2022-04-18 13:46:47 +0200

    add ar keys for client,attachment type

--- app/Http/Resources/Dashboard/ClientResource.php
+++ app/Http/Resources/Dashboard/ClientResource.php
@@ -15,6 +15,7 @@ class ClientResource extends JsonResource
      */
     public function toArray($request)
     {
+        $map = [null => null, "company" => "شركات", "institution" => "مؤسسة", "other" => "أخري", "member" => "عضو", "freelance_doc" => "مستقل", "famous" => "مشهور"];
         return [
             'user' => SimpleUserResource::make($this->whenLoaded('user')),
             "tax_number" => $this->tax_number,
@@ -25,9 +26,9 @@ class ClientResource extends JsonResource
             "nationality" => $this->nationality,
             "address" => $this->address,
             "marital_status" => $this->marital_status,
-            'client_type' => $this->client_type,
-            "register_type"=>$this->register_type,
-            "transactions_done"=>isset($this->transactions_done)?$this->transactions_done:0,
+            'client_type' => ["ar" => $map[$this->client_type], "en" => $this->client_type],
+            "register_type" => $this->register_type,
+            "transactions_done" => isset($this->transactions_done) ? $this->transactions_done : 0,
             'created_at' => $this->created_at,
             'token' => $this->when($this->token, $this->token),
             'managers' => ManagerResource::collection($this->whenLoaded('managers')),
