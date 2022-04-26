9a9d6750103ab74fe8f2fcc228aefa9f73bd833f
Taha <taha.mohamed2016@gmail.com>
2022-04-26 12:04:21 +0200

    Edit Code Style

--- resources/views/dashboard/transaction/script.blade.php
+++ resources/views/dashboard/transaction/script.blade.php
@@ -205,8 +205,6 @@
                   $('#branchProgram').empty();
                   $("#branchProgram").append('<option value=""> {{ trans('dashboard.activity_log.select_subprogram') }} </option>')
                   if (mainprog_id != '') {
-
-
                       //send ajax
                       $.ajax({
                           url: '{{ url('dashboard/activitylog/sub-programs') }}' + '/' + mainprog_id,
@@ -225,33 +223,28 @@
               });
  //get employees from Department
  $("#mainDepartment").change(function(e) {
-                  e.preventDefault();
-                  let maindep_id = $("#mainDepartment").val();
-                  $('#employee').empty();
-                  $("#employee").append('<option value=""> {{ trans('dashboard.activity_log.select_employee') }} </option>')
-                  if (maindep_id != '') {
-
-
-                      //send ajax
-                      $.ajax({
-                          url: '/dashboard/activitylog/all-employees/' + maindep_id,
-                          type: 'get',
-                          success: function(data) {
-                              if (data) {
-                                  $.each(data.data, function(index, user_id) {
-                                      $("#employee").append('<option value="' + user_id.id +
-                                          '">' + user_id.fullname + '</option>')
-                                  });
-
-                              }
-                          }
+      e.preventDefault();
+      let maindep_id = $("#mainDepartment").val();
+      $('#employee').empty();
+      $("#employee").append('<option value=""> {{ trans('dashboard.activity_log.select_employee') }} </option>')
+      if (maindep_id != '') {
+          //send ajax
+          $.ajax({
+              url: '{{ url('dashboard/activitylog/all-employees') }}/' + maindep_id,
+              type: 'get',
+              success: function(data) {
+                  if (data) {
+                      $.each(data.data, function(index, user_id) {
+                          $("#employee").append('<option value="' + user_id.id +
+                              '">' + user_id.fullname + '</option>')
                       });
+
                   }
-
-              });
-
-
-        });
+              }
+          });
+       }
+    });
+});
 </script>
 
 {{-- <script>
