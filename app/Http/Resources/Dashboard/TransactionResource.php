09a34b496f63dc9203b744a277038010f68d9e5a
Mohamed Karem <karem.fintech@gmail.com>
2022-05-10 10:41:05 +0200

    edit transactions api

--- app/Http/Resources/Dashboard/TransactionResource.php
+++ app/Http/Resources/Dashboard/TransactionResource.php
@@ -17,15 +17,16 @@ class TransactionResource extends JsonResource
         return [
             'id' => $this->id,
             'number' => $this->number,
-            'amount' => $this->amount,
-            'status' => $this->status,
-            'type' => $this->type,
             'created_at' => $this->created_at,
-            'transaction_id' => $this->transaction_id,
-            'transaction_data' => $this->transaction_data,
-            'citizen' => CitizenResource::make($this->whenLoaded('citizen')),
-            'client' => ClientResource::make($this->whenLoaded('client')),
-            'card' => CardPackageResource::make($this->whenLoaded('card')),
+            'citizen' => $this->citizen?->fullname,
+            'user_identity' => $this->user_identity,
+            'client' => $this->client?->user?->fullname,
+            'amount' => (string)$this->amount,
+            'total_amount' => (string)$this->total_amount,
+            'gift_balance' => (string)$this->gift_balance,
+            'type' => $this->type ? trans("dashboard.transaction.type_cases.{$this->type}") : "",
+            'status' => $this->status ? trans("dashboard.transaction.status_cases.{$this->status}") : "",
+            'discount_percent' => $this->card->name.' / '.$this->discount_percent.'%',
         ];
     }
 }
