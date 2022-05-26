db3a31dccc722b255b7a239e59cadbe4e873aaf6
Taha <taha.mohamed2016@gmail.com>
2022-05-26 11:13:28 +0200

    Edit Export

--- resources/views/dashboard/department/export.blade.php
+++ resources/views/dashboard/department/export.blade.php
@@ -3,15 +3,15 @@
 
 <head>
   <meta charset="UTF-8" />
+  <link href="{{ asset('dashboardAssets/css/style.css') }}" rel="stylesheet" />
   <style>
     @media screen,
     print {
       * {
         -webkit-print-color-adjust: exact;
-
       }
 
-      header {
+      .header {
         margin-bottom: 2em;
         background: url("{{ asset('dashboardAssets/images/brand/fot-04.svg') }}") no-repeat;
         background-size: contain;
@@ -19,18 +19,18 @@
         position: relative
       }
 
-      header h3 {
+      .header h3 {
         text-align: left;
         width: 100%;
         font-weight: bold;
         line-height: 1.5em
       }
 
-      header p {
+      .header p {
         margin: 0
       }
 
-      header b {
+      .header b {
         font-weight: bold
       }
 
@@ -42,7 +42,7 @@
         width: 100%
       }
 
-      footer {
+      .footer {
         margin-bottom: 2em;
         background: url("{{ asset('dashboardAssets/images/brand/header-05.svg') }}") no-repeat;
         background-size: cover;
@@ -50,10 +50,8 @@
         position: relative
       }
 
-      .table-bordered th,
-      .text-wrap table th,
-      .table-bordered td,
-      .text-wrap table td {
+      th,
+      td{
         border: 1px solid #e9edf4;
       }
 
@@ -86,75 +84,59 @@
       td:last-of-type {
         border-left: 1px solid #e9edf4;
       }
-
-      .active {
-        color: #04A777
-      }
-
-      .unactive {
-        color: #e23e3d
-      }
     }
   </style>
 </head>
 
-<body class="app sidebar-mini {{ LaravelLocalization::getCurrentLocaleDirection() }}">
-  <div class="container">
-    <div class="row row-sm">
-      <div class="col-lg-12">
-        <table id="departmentTable" class="table">
-          <thead>
-            <tr>
-              <th colspan="5" style="border: none;">
-                <div class="col-6 ms-md-auto" style="transform: translateY(100%);text-align: left;">
-                  <h3>تقرير عن الأقسام </h3>
-                  <p>تاريخ إنشائها من (20/03/2023) إلى (25/03/2022)</p>
+<body>
+  <table>
+    <thead>
+      <tr>
+        <th colspan="5" style="border: none;">
+            <div class="header">
+                <div style="transform: translateY(100%);text-align: left;">
+                    <h3>تقرير عن الأقسام </h3>
+                    <p>تاريخ إنشائها من (20/03/2023) إلى (25/03/2022)</p>
                 </div>
-                <div class="col-5 ms-md-auto"
-                  style="position: absolute; left: 0; padding: 0; text-align: left; top: 65%; transform: translateY(100%);">
-                  <div class="row">
-                    <div class="col-6">
-                      <b>رقم المستخدم: </b>256324
-                    </div>
-                    <div class="col-6">
-                      <b>تاريخ الطباعة: </b>{{ now() }}
-                    </div>
-                  </div>
-                </div>
-              </th>
-            </tr>
-            <tr>
-              <th class="border-bottom-0">#</th>
-              <th class="border-bottom-0">
-                @lang('dashboard.department.department')</th>
-              <th class="border-bottom-0">
-                @lang('dashboard.department.main_department')</th>
-              <th class="border-bottom-0">
-                @lang('dashboard.general.created_at')</th>
-              <th class="border-bottom-0">
-                @lang('dashboard.general.status')</th>
-            </tr>
-          </thead>
-          <tbody>
-            @foreach ($departments as $department)
-            <tr>
-              <td>{{ $loop->iteration }}</td>
-              <td>{{ $department->name }}</td>
-              <td>{{ @$department->parent->name ?? null }}</td>
-              <td>{{ $department->created_at }}</td>
-              <td>
-                @if($department->is_active)
-                  {{ trans('dashboard.general.active_cases.1') }}
-                @else
-                  {{ trans('dashboard.general.active_cases.0') }}
-                @endif
-              </td>
-            </tr>
-            @endforeach
-          </tbody>
-        </table>
+                <div style="position: absolute; left: 0; padding: 0; text-align: left; top: 65%; transform: translateY(100%);">
+            <div style="width: 50%;">
+              <b>رقم المستخدم: </b>256324
+            </div>
+            <div style="width: 50%;">
+              <b>تاريخ الطباعة: </b> {{ date("Y-m-d H:i") }}
+            </div>
+          </div>
       </div>
-    </div>
+        </th>
+      </tr>
+      <tr>
+        <th>#</th>
+        <th>
+          @lang('dashboard.department.department')</th>
+        <th>
+          @lang('dashboard.department.main_department')</th>
+        <th>
+          @lang('dashboard.general.created_at')</th>
+        <th>
+          @lang('dashboard.general.status')</th>
+      </tr>
+    </thead>
+    <tbody>
+      @foreach ($departments as $department)
+      <tr>
+        <td>{{ $loop->iteration }}</td>
+        <td>{{ $department->name }}</td>
+        <td>{{ @$department->parent->name ?? null }}</td>
+        <td>{{ $department->created_at }}</td>
+        <td>
+          {{ trans('dashboard.general.active_cases.'.$department->is_active) }}
+        </td>
+      </tr>
+      @endforeach
+    </tbody>
+  </table>
+  <div class="footer">
+
   </div>
 </body>
 
