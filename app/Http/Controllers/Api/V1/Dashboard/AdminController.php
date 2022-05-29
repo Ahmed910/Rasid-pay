6cfdf14812488aa68b5c47bab9981ce3ef4833f8
Mohamed Fathi <mohamedkdr66@gmail.com>
2022-05-29 14:08:48 +0200

    Add Hot Fix

--- app/Http/Controllers/Api/V1/Dashboard/AdminController.php
+++ app/Http/Controllers/Api/V1/Dashboard/AdminController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\V1\Dashboard\AdminRequest;
 use App\Http\Requests\V1\Dashboard\ReasonRequest;
 use App\Http\Resources\Dashboard\{UserResource, Admin\AdminCollection};
-use App\Models\{User, Group\Group, Employee};
+use App\Models\{Admin, User, Group\Group, Employee};
 use App\Models\Department\Department;
 use Illuminate\Http\Request;
 
@@ -16,11 +16,11 @@ class AdminController extends Controller
     public function index(Request $request)
     {
         $users = User::CustomDateFromTo($request)
-        ->has('employee')
-        ->search($request)
-        ->with(['department', 'permissions', 'groups' => function ($q) {
-            $q->with('permissions');
-        }])->where('user_type', 'admin')
+            ->has('employee')
+            ->search($request)
+            ->with(['department', 'permissions', 'groups' => function ($q) {
+                $q->with('permissions');
+            }])->where('user_type', 'admin')
             ->sortBy($request)
             ->paginate((int)($request->per_page ?? config("globals.per_page")));
 
@@ -56,16 +56,18 @@ class AdminController extends Controller
     }
 
 
-    public function store(AdminRequest $request)
+    public function store(AdminRequest $request,User $admin)
     {
-        $admin = User::where('user_type', 'employee')->findOrFail($request->employee_id);
-        $admin->fill(['user_type' => 'admin', 'password' => $request->password, 'added_by_id' => auth()->id(), 'is_login_code' => $request->is_login_code, 'login_id' => $request->login_id])->save();
+        $admin->fill([
+            'user_type' => 'admin', 'password' => $request->password, 'added_by_id' => auth()->id(),
+            'is_login_code' => $request->is_login_code, 'login_id' => $request->login_id
+        ])->save();
         $employee = Employee::create($request->safe()->only(['department_id', 'rasid_job_id']) + ['user_id' => $admin->id]);
         $employee->job()->update(['is_vacant' => 0]);
         $admin->admin()->create();
         //TODO::send sms with password
         $permissions = $request->permission_list ?? [];
-        if ($request->group_list) {
+    if ($request->group_list) {
             $admin->groups()->sync($request->group_list);
             $permissions = array_filter(array_merge($permissions, Group::find($request->group_list)->pluck('permissions')->flatten()->pluck('id')->toArray()));
         }
