a854ffb7d69c55acb54887269a77690ec9261d2c
Mohamed Fathi <mohamedkdr66@gmail.com>
2022-06-05 11:08:00 +0200

    Add Routes

--- routes/v1/mobile.php
+++ routes/v1/mobile.php
@@ -14,3 +14,23 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::post('send-message', 'ContactController@sendMessage')->name('send_message');
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('logout', 'Auth\LoginController@logout');
+    Route::apiResource('profile', 'ProfileController')->only('index','store');
+    Route::post('update_password', 'ProfileController@updatePassword');
+    Route::get('get_citizen_wallet', 'WalletController@getCitizenWallet');
+});
+
+Route::get('slides','SlideController@index');
+Route::get('banks','BankController@index');
+
+Route::controller('Auth\LoginController')->group(function () {
+    Route::post('login', 'login')->name('login');
+    Route::post('send_reset_code', 'sendResetCode');
+});
+
+Route::controller('Auth\RegisterController')->group(function () {
+    Route::post('register', 'register');
+    Route::post('check-verification-code', 'checkVerificationCode');
+});
