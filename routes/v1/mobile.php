44ab675649db2eff19dbf4e350478670b9e73351
Mohamed Karem <karem.fintech@gmail.com>
2022-06-06 14:33:47 +0200

    fitch wallet with last updates / add last updated for citizen wallet / remove duplicated mobile routes

--- routes/v1/mobile.php
+++ routes/v1/mobile.php
@@ -17,17 +17,21 @@ Route::post('send-message', 'ContactController@sendMessage')->name('send_message
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('logout', 'Auth\LoginController@logout');
-    Route::apiResource('profiles', 'ProfileController')->only('index','store');
-    Route::apiResource('notifications', 'NotificationController')->only('index','show');
+    Route::apiResource('profiles', 'ProfileController')->only('index', 'store');
     Route::post('update_password', 'ProfileController@updatePassword');
+    // notifications
+    Route::apiResource('notifications', 'NotificationController')->only('index', 'show');
+    Route::post('activate_notification', 'ProfileController@activateNotification');
+    // home
+    Route::get('home', 'HomeController@index');
+    // fitch wallet
+    Route::get('fitch_wallet', 'HomeController@fitchWallet');
+    // citizen wallet
     Route::get('get_citizen_wallet', 'WalletController@getCitizenWallet');
-    Route::post('MoneyRequests','MoneyRequestController@store');
-    Route::post('activate_notifcation', 'ProfileController@activateNotifcation');
 
+    //money requests
+    Route::post('MoneyRequests', 'MoneyRequestController@store');
 });
-
-Route::get('slides','SlideController@index');
-Route::get('banks','BankController@index');
 
 Route::controller('Auth\LoginController')->group(function () {
     Route::post('login', 'login')->name('login');
@@ -43,19 +47,6 @@ Route::controller('Auth\RegisterController')->group(function () {
 Route::controller('Auth\ResetController')->group(function () {
     Route::post('check-identity-number', 'checkIdentityNumber');
     Route::post('reset-password', 'updatePassword');
-});
-Route::middleware('auth:sanctum')->group(function () {
-    Route::post('logout', 'Auth\LoginController@logout');
-    Route::apiResource('profile', 'ProfileController')->only('index', 'store');
-    Route::post('update_password', 'ProfileController@updatePassword');
-    Route::post('activate_notification', 'ProfileController@activateNotification');
-
-    // home
-    Route::get('home', 'HomeController@index');
-
-    // citizen wallet
-    Route::get('get_citizen_wallet', 'WalletController@getCitizenWallet');
-
 });
 
 Route::get('slides', 'SlideController@index');
