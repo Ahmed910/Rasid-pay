57c25d6fdbbb2b31ea50eb45f8583b5c7340ca34
Mohamed Fathi <mohamedkdr66@gmail.com>
2022-06-07 11:19:32 +0200

    Fix Issues In Register

--- app/Http/Requests/V1/Mobile/Auth/CompleteRegisterRequest.php
+++ app/Http/Requests/V1/Mobile/Auth/CompleteRegisterRequest.php
@@ -10,7 +10,7 @@ class CompleteRegisterRequest extends ApiMasterRequest
     public function rules()
     {
         return [
-            'phone'    => ['required', 'string', 'digits_between:5,20', 'starts_with:9665,05', 'regex:/^(05|9665)([0-9]{8})$/', 'exits:users,phone'],
+            'phone'    => ['required', 'string', 'digits_between:9,20', 'starts_with:9665,05', 'regex:/^(05|9665)([0-9]{8})$/', 'exists:users,phone'],
             'password' => 'required|min:8|regex:/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{6,}$/'
         ];
     }
