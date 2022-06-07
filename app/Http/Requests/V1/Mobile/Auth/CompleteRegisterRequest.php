3c3f03f1d949f0a3b619d670c9b89b90eefff5eb
Mohamed Fathi <mohamedkdr66@gmail.com>
2022-06-07 12:56:19 +0200

    Fix identity_number exists on users

--- app/Http/Requests/V1/Mobile/Auth/CompleteRegisterRequest.php
+++ app/Http/Requests/V1/Mobile/Auth/CompleteRegisterRequest.php
@@ -10,7 +10,7 @@ class CompleteRegisterRequest extends ApiMasterRequest
     public function rules()
     {
         return [
-            'identity_number' => 'required|numeric|digits_between:10,20|unique:users,identity_number',
+            'identity_number' => 'required|numeric|digits_between:10,20|exists:users,identity_number',
             'password' => 'required|min:8|regex:/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{6,}$/'
         ];
     }
