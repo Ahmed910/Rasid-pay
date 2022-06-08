f625d241afc26581c9855e93ebc111c019834fb7
Mohamed Karem <karem.fintech@gmail.com>
2022-06-08 12:21:59 +0200

    show packages api

---
+++ app/Http/Resources/Mobile/PackageResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources\Mobile;
+
+use Carbon\Carbon;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PackageResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'description' => $this->description,
+            'color' => $this->color,
+            'price' => $this->price,
+            'discount' => $this->discount,
+            'expire_at' => Carbon::now()->addMonths($this->duration)->format('Y/m/d'),
+        ];
+    }
+}
