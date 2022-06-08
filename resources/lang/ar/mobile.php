5b2d4489f5e577d8e22146b45501006e6b4682ac
AhmadE920 <ahmed256.fintech@gmail.com>
2022-06-08 11:04:53 +0200

    create tranlation files for mobile

---
+++ resources/lang/ar/mobile.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'local_transfers'=>[
+        'local_transfers'=>'التحويلات المحلية',
+        'local_transfer'=>'التحويل المحلي',
+        'current_balance_is_not_sufficiant_to_complete_transaction'=>'الرصيد الحالي لا يكفي لإجراء عملية التحويل',
+    ]
+];
---
+++ resources/lang/en/mobile.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'local_transfers'=>[
+        'local_transfers'=>'Local Transfers',
+        'local_transfer'=>'Local Transfer',
+        'current_balance_is_not_sufficiant_to_complete_transaction'=>'Current Balance Is Not Sufficiant To Complete Transaction',
+    ]
+];
