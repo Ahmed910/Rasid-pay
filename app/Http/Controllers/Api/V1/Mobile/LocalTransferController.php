f6d72ae3966a92e182996f4f11872d4f379c7b1c
AhmadE920 <ahmed256.fintech@gmail.com>
2022-06-08 11:03:05 +0200

    store transfers

--- app/Http/Controllers/Api/V1/Mobile/LocalTransferController.php
+++ app/Http/Controllers/Api/V1/Mobile/LocalTransferController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api\V1\Mobile;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\V1\Mobile\LocalTransferRequest;
+use App\Models\CitizenWallet;
 use App\Models\Transfer;
 use Illuminate\Http\Request;
 
@@ -12,7 +13,12 @@ class LocalTransferController extends Controller
     public function store(LocalTransferRequest $request)
     {
         // check main_balance is suffienct or not
-       $local_transfer = Transfer::create($request->only('amount','transfer_fees')+['transfer_type'=>'local']);
+        $main_balance_for_current_user = CitizenWallet::where('citizen_id',auth()->id())->select('main_balance')->firstOrFail();
+        if($main_balance_for_current_user->main_balance < $request->amount)
+        {
+            return response()->json(['data'=>'','message'=>trans('mobile.local_transfers.current_balance_is_not_sufficiant_to_complete_transaction'),'status'=>'error'],422);
+        }
+       $local_transfer = Transfer::create($request->only('amount','transfer_fees')+['transfer_type'=>'local','from_user_id'=>auth()->id]);
        $local_transfer->bank_transfer()->create($request->except('amount','transfer_fees'));
     }
 }
