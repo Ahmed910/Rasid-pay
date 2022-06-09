6937dcbe4aa522fb60c055fec12d5f27adc24f31
Mohamed Karem <karem.fintech@gmail.com>
2022-06-09 13:23:15 +0200

    edit enabled packages for citizens

--- app/Http/Resources/Blade/Dashboard/Package/PackageResource.php
+++ app/Http/Resources/Blade/Dashboard/Package/PackageResource.php
@@ -2,8 +2,6 @@
 
 namespace App\Http\Resources\Blade\Dashboard\Package;
 
-use App\Http\Resources\Dashboard\SimpleUserResource;
-use App\Models\Package\Package;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class PackageResource extends JsonResource
@@ -22,12 +20,11 @@ class PackageResource extends JsonResource
                 $package_discount[$clientPackage->name] = $clientPackage->pivot->package_discount;
             }
         }
-
         return [
             "fullname" => $this->fullname,
-            "basic_discount" => array_key_exists(trans('dashboard.cardpackage.basic'), $package_discount) ? $package_discount[trans('dashboard.cardpackage.basic')] : trans('dashboard.package.without') ,
-            "golden_discount" => array_key_exists(trans('dashboard.cardpackage.golden'), $package_discount) ? $package_discount[trans('dashboard.cardpackage.golden')] :trans('dashboard.package.without') ,
-            "platinum_discount" => array_key_exists(trans('dashboard.cardpackage.platinum'), $package_discount) ? $package_discount[trans('dashboard.cardpackage.platinum')] : trans('dashboard.package.without') ,
+            "basic_discount" => array_key_exists(trans('dashboard.cardpackage.basic'), $package_discount) ? $package_discount[trans('dashboard.cardpackage.basic')] : 0,
+            "golden_discount" => array_key_exists(trans('dashboard.cardpackage.golden'), $package_discount) ? $package_discount[trans('dashboard.cardpackage.golden')] : 0,
+            "platinum_discount" => array_key_exists(trans('dashboard.cardpackage.platinum'), $package_discount) ? $package_discount[trans('dashboard.cardpackage.platinum')] : 0,
             "edit_route" => route("dashboard.package.edit", $this->id),
             'start_from' => $request->start
         ];
