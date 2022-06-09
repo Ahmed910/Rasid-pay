59a71ba9364cb21e317363ecfc31ee3ece16c8c1
Taha <taha.mohamed2016@gmail.com>
2022-06-09 14:47:01 +0200

    Edit Client Packages

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -50,7 +50,7 @@ class Transaction extends Model
         parent::boot();
        static::created(function ($item) {
            $qr_code = self::createQr($item->trans_number);
-           $item->update(['qr_code' => $qr_code]);
+           $item->update(['qr_path' => $qr_code]);
        });
     }
 
@@ -67,7 +67,7 @@ class Transaction extends Model
             ->encoding('UTF-8')
             ->merge(public_path('dashboardAssets/images/brand/logoQR.png'), .2, true)
             ->size(500)
-            ->generate($qr_value, $path);
+            ->generate((string)$qr_value, $path);
         return 'images/transactions/' . $filname;
     }
 
