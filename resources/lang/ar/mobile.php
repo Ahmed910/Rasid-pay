3b54b559659ac6de3f34cb4606a90e1f997958e1
AhmadE920 <ahmed256.fintech@gmail.com>
2022-06-09 16:45:19 +0200

    enhance wallet charge code

--- resources/lang/ar/mobile.php
+++ resources/lang/ar/mobile.php
@@ -1,10 +1,16 @@
 <?php
 
 return [
-    'local_transfers'=>[
-        'local_transfers'=>'التحويلات المحلية',
-        'local_transfer'=>'التحويل المحلي',
-        'current_balance_is_not_sufficiant_to_complete_transaction'=>'الرصيد الحالي لا يكفي لإجراء عملية التحويل',
-        'transfer_has_been_done_successfully'=>'تمت عملية التحويل بنجاح'
-    ]
+    'local_transfers' => [
+        'local_transfers' => 'التحويلات المحلية',
+        'local_transfer' => 'التحويل المحلي',
+        'current_balance_is_not_sufficiant_to_complete_transaction' => 'الرصيد الحالي لا يكفي لإجراء عملية التحويل',
+        'transfer_has_been_done_successfully' => 'تمت عملية التحويل بنجاح'
+    ],
+    'messages' => [
+        'wallet_bin_has_been_updated' => 'تم تحديث الرقم الخاص بالمحفظة',
+        'firstly_add_wallet_bin' => 'يجب إضافة رقم المحفظة أولاً',
+        'you_can_complete_your_transaction'=>'يمكنك إستكمال العملية',
+        'your_tries_have_been_expired'=>'لقد تم إنتهاء محاولاتك لإتمام العميلة'
+    ],
 ];
