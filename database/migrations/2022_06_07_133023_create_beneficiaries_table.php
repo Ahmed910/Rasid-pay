65483217124c83ec1f8f53986e435450d224daf4
Mohamed Fathi <mohamedkdr66@gmail.com>
2022-06-09 12:40:24 +0200

    (Migrations) Fix beneficiaries migrations and model relations

--- database/migrations/2022_06_07_133023_create_beneficiaries_table.php
+++ database/migrations/2022_06_07_133023_create_beneficiaries_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Beneficiary;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -19,12 +20,11 @@ class CreateBeneficiariesTable extends Migration
             $table->foreignUuid('country_id')->nullable()->constrained("countries")->onDelete('set null');
             $table->foreignUuid('user_id')->nullable()->constrained('users')->onDelete('set null');
             $table->foreignUuid('recieve_option_id')->nullable()->constrained('recieve_options')->onDelete('set null');
-            $table->foreignUuid('nationality_id')->nullable()->constrained('countries')->onDelete('set null');
+            $table->string('nationality')->nullable();
             $table->date('date_of_birth')->nullable();
-            $table->enum('benficiar_type', ['local', 'global']);
+            $table->enum('benficiar_type', Beneficiary::TYPES);
             $table->string('iban_number')->nullable();
-            $table->string('address');
-            $table->string('relation');
+            $table->string('relation')->nullable();
 
             $table->softDeletes();
             $table->timestamps();
