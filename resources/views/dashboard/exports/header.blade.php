f2d4138adc30ebaffd9773964cb477822ffc7520
Mohamed Fathi <mohamedkdr66@gmail.com>
2022-06-09 15:07:17 +0200

    Format Exports header

--- resources/views/dashboard/exports/header.blade.php
+++ resources/views/dashboard/exports/header.blade.php
@@ -1,18 +1,16 @@
-            <tr>
-                <th colspan="2" style="padding-right: 1em; padding-left: 1em; text-align: center">
-
-                    <img src="{{ public_path('dashboardAssets/images/brand/fintech.png') }}" width="150" style="margin: auto" alt="">
-
-                </th>
-                <th colspan="3">
-                    <h3>تقرير عن {{ $topic }}
-                    </h3>
-                    <br>
-                    <p>تاريخ إنشائها من (20/03/2023) إلى (25/03/2022)</p>
-                    <br>
-                    <p>رقم المستخدم: 256324</p>
-                    <br>
-                    <p>تاريخ الطباعة: 20/03/2022</p>
-                    <br>
-                </th>
-            </tr>
+<tr>
+  <th colspan="2" style="padding-right: 1em; padding-left: 1em; text-align: center">
+    <img src="{{ public_path('dashboardAssets/images/brand/fintech.png') }}" width="150" style="margin: auto" alt="">
+  </th>
+  <th colspan="3">
+    <h3>تقرير عن {{ $topic }}
+    </h3>
+    <br>
+    <p>تاريخ إنشائها من (20/03/2023) إلى (25/03/2022)</p>
+    <br>
+    <p>رقم المستخدم: 256324</p>
+    <br>
+    <p>تاريخ الطباعة: 20/03/2022</p>
+    <br>
+  </th>
+</tr>
