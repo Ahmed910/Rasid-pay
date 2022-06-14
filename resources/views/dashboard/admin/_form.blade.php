a5afb5435d03ea716619950700c2195e3881108d
Mohamed Fathi <mohamedkdr66@gmail.com>
2022-06-14 10:54:08 +0200

    Fix issue IS453

--- resources/views/dashboard/admin/_form.blade.php
+++ resources/views/dashboard/admin/_form.blade.php
@@ -15,7 +15,7 @@
       $admin->employee?->department_id :
       null, ['class' => 'form-control select2-show-search', 'data-placeholder' =>
       trans('dashboard.department.select_department'),'id' => 'mainDepartment', 'onchange' =>
-      'getJobs(this.value)']) !!}
+      'getJobs(this.value)' , !($admin->exists()) ?: 'disabled']) !!}
       <span class="text-danger" id="department_idError"></span>
 
     </div>
@@ -25,7 +25,7 @@
       <div id="new_admin">
         {!! Form::select('rasid_job_id', isset($admin) ? ['' => ''] + $rasid_jobs :['' => ''] , isset($admin) ?
         $admin->employee?->rasid_job_id :null, ['class' => 'form-control select2-show-search', 'id' =>
-        'rasid_job_id', 'data-placeholder' => trans('dashboard.rasid_job.select_job')]) !!}
+        'rasid_job_id', 'data-placeholder' => trans('dashboard.rasid_job.select_job'),!($admin->exists()) ?: 'disabled']) !!}
       </div>
       <span class="text-danger" id="rasid_job_idError"></span>
     </div>
