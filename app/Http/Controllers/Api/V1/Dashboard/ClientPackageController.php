77cb69b0ecd42b24bd2cf432ae6c7ff01e5444d2
Mohamed Karem <karem.fintech@gmail.com>
2022-06-15 15:27:37 +0200

    add main packages endpoint for client packages - edit response (vue)

--- app/Http/Controllers/Api/V1/Dashboard/ClientPackageController.php
+++ app/Http/Controllers/Api/V1/Dashboard/ClientPackageController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\V1\Dashboard\CardPackageRequest;
 use App\Http\Requests\V1\Dashboard\CardPackageUpdateRequest;
 use App\Http\Resources\Dashboard\PackageResource;
-use App\Http\Resources\Mobile\PackageResource as MainPackageResource;
+use App\Http\Resources\Dashboard\MainPackageResource;
 use App\Http\Resources\Dashboard\SimpleUserResource;
 use App\Models\Package\Package;
 use App\Models\User;
