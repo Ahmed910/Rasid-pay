2590aef993d7a5b0a7d77c8f644f714efe892fd6
HanyaHesham <hanyahesham8@gmail.com>
2022-06-16 11:19:18 +0200

    updates

--- resources/views/dashboard/group/_form.blade.php
+++ resources/views/dashboard/group/_form.blade.php
@@ -27,10 +27,11 @@
                 <select name="permission_list[]" hidden multiple></select>
                 <select name="group_list[]" hidden multiple></select>
 
-                <select class="form-control select2" onchange="addPermissions(this.selectedOptions)"
+                <select class="form-control select2 multipleSelect" onchange="addPermissions(this.selectedOptions)"
                     data-placeholder="{{ trans('dashboard.general.select_permissions') }}" multiple="multiple"
                     id="permissions" required>
-                    <option value="all">إختر الكل</option>
+                    <option value="selectAll" class="selectAll">إختر الكل</option>
+                    <option value="unselectAll" class="unselectAll" >إلغاء تحديد الكل</option>
                     @foreach ($groups as $id => $name)
                         <option value="{{ $id }}" data-name="groups"
                             {{ isset($group) && in_array($id, $group->group_list) ? 'selected' : null }}>
@@ -139,38 +140,39 @@
 
 
         $(document).ready(function() {
-            $(".select2").select2({
+            $(".multipleSelect").select2({
                 closeOnSelect: false
             });
-            $(".select2").select2({
+            $(".multipleSelect").select2({
                 closeOnSelect: false,
                 templateResult: formatState
-
             });
-
-            $('.select2').on("select2:select", function(e) {
+            
+            $('.multipleSelect').on("select2:select", function(e) {
                 var data = e.params.data.text;
-                if (data == 'إختر الكل') {
-                    $(".select2 > option").prop("selected", "selected");
-                    $(".select2").trigger("change");
+                if (data == 'إختر الكل'){
+                    $(".multipleSelect > option").prop("selected", "selected");
+                    $(".multipleSelect").trigger("change");
                     $(".custom-checkbox input").prop("checked", "checked");
-                    $("label[for='all'] .custom-control-label").text("إلغاء تحديد الكل");
+                    $("label[for='selectAll']").parent().hide();
+                    $("label[for='unselectAll']").parent().show();
                 }
-                // elseif (data == 'إلغاء تحديد الكل') {
-                //     $(".select2 > option").prop("selected", "");
-                //     $(".select2").trigger("change");
-                //     $(".custom-checkbox input").prop("checked", "");
-                //     $("label[for='all'] .custom-control-label").text("إختر الكل");
-                // }
+                else if (data == 'إلغاء تحديد الكل'){
+                    $(".multipleSelect > option").prop("selected", "");
+                    $(".multipleSelect").trigger("change");
+                    $(".custom-checkbox input").prop("checked", "");
+                    $("label[for='unselectAll']").parent().hide();
+                    $("label[for='selectAll']").parent().show();
+                }
             });
         });
+
 
         function formatState(state) {
             console.log(state, 'state')
             if (!state.id) {
                 return state.text;
             }
-
             var $state = $(
                 `<label for="${state.id}" class="d-flex justify-content-between align-items-center m-0">
                   <div class="">
