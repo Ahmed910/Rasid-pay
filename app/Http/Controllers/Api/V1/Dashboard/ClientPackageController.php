1e7a77ded619912776e3f2dc22423b31c8e6c8f9
Mohamed Karem <karem.fintech@gmail.com>
2022-06-16 11:10:29 +0200

    edit store client package request (vue)

--- app/Http/Controllers/Api/V1/Dashboard/ClientPackageController.php
+++ app/Http/Controllers/Api/V1/Dashboard/ClientPackageController.php
@@ -45,7 +45,7 @@ class ClientPackageController extends Controller
         ]);
     }
 
-    public function store(CardPackageUpdateRequest $request)
+    public function store(CardPackageRequest $request)
     {
         $client = User::where('user_type', 'client')->findOrFail($request->client_id);
         $client->clientPackages()->sync($request->discounts);
