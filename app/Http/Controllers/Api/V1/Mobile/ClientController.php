0ffe9dca73a4a89846eab0b5a211dfda9e49014a
Mohamed Karem <karem.fintech@gmail.com>
2022-06-19 11:38:47 +0200

    enhance show clients api

--- app/Http/Controllers/Api/V1/Mobile/ClientController.php
+++ app/Http/Controllers/Api/V1/Mobile/ClientController.php
@@ -11,16 +11,10 @@ class ClientController extends Controller
 {
     public function index(Request $request)
     {
-        $keyword = $request->search;
-        $clients = User::where(['user_type'=>'client', 'ban_status' => 'active'])->has('clientPackages')->with('clientPackages','client')->when($keyword,function ($query)  use($keyword) {
-            $query->where(function ($q) use($keyword) {
-                $q->where('fullname', 'like', '%' . $keyword . '%')
-                   ->orWhere('email', 'like', '%' . $keyword . '%')
-                   ->orWhere('phone', 'like', '%' . $keyword . '%');
-               });
-            })
+        $clients = User::where(['user_type'=>'client', 'ban_status' => 'active'])
+            ->has('clientPackages')->with('clientPackages','client')
+            ->search($request)
         ->paginate((int)($request->per_page ?? config("globals.per_page")));
-
         return ClientResource::collection($clients)->additional([
             'status'=>true,
             'message'=>''
@@ -31,8 +25,7 @@ class ClientController extends Controller
     public function show($id)
     {
         $client = User::has('clientPackages')->where(['user_type'=>'client', 'ban_status' => 'active'])->with('clientPackages','client')->findOrFail($id);
-        return
-        ClientResource::make($client)->additional([
+        return ClientResource::make($client)->additional([
             'status' => true,
             'message' => ''
         ]);
