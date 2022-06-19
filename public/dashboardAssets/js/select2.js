b67320feed082fe6006626356bcc6492b7601b36
AhmadE920 <ahmed256.fintech@gmail.com>
2022-06-19 15:13:36 +0200

    show error validation when blur about select box

--- public/dashboardAssets/js/select2.js
+++ public/dashboardAssets/js/select2.js
@@ -2,17 +2,20 @@ $(function(e) {
     'use strict'
 
     // Select2
+
     $('.select2').select2({
         minimumResultsForSearch: Infinity,
         width: '100%',
         language: { noResults: () => "لاتوجد نتائج متاحة"}
-    });
+    })
     // Select2 by showing the search
     $('.select2-show-search').select2({
         minimumResultsForSearch: '',
         width: '100%',
         language: { noResults: () => "لاتوجد نتائج متاحة"}
-    });
+    }).on("select2:closing", function(e) {
+        validateData(this)
+      });
     $('.select2').on('click', () => {
         let selectField = document.querySelectorAll('.select2-search__field')
         selectField.forEach((element, index) => {
