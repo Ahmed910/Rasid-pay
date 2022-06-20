cf26c592b1b61d040743040f36c1f9dccafd9bab
Mohamed Fathi <mohamedkdr66@gmail.com>
2022-06-20 11:22:29 +0200

    Generate pdf file and download it in transaction

--- app/Http/Controllers/Api/V1/Mobile/TransactionController.php
+++ app/Http/Controllers/Api/V1/Mobile/TransactionController.php
@@ -4,7 +4,10 @@ namespace App\Http\Controllers\Api\V1\Mobile;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\V1\Mobile\TransactionRequest;
-use App\Http\Resources\Api\V1\Mobile\Transactions\TransactionResource;
+use App\Http\Resources\Mobile\Transactions\TransactionResource;
+use App\Services\GeneratePdf;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
 
 class TransactionController extends Controller
 {
@@ -34,5 +37,29 @@ class TransactionController extends Controller
                 'status' => true,
                 'message' => 'success'
             ]);
+    }
+
+    public function generatePdfFile($id, GeneratePdf $generatePdfFile)
+    {
+        $this->checkInvoicesFolderExists();
+        $transaction = auth()->user()->citizenTransactions()->findOrFail($id);
+
+        if ($transaction->summary_path && is_file(storage_path('app/' . $transaction->summary_path)))
+            return Storage::download($transaction->summary_path);
+
+        $path =  $generatePdfFile->newFile()
+            ->view('dashboard.exports.mobile.invoice', ['invoice' => $transaction])
+            ->storeOnLocal('invoices/');
+
+        $transaction->update(['summary_path' => $path]);
+
+        return Storage::download($path);
+    }
+
+    private function checkInvoicesFolderExists()
+    {
+        if (!File::isDirectory(storage_path('app/invoices'))) {
+            File::makeDirectory(storage_path('app/invoices'), 0777, true);
+        }
     }
 }
---
+++ resources/views/dashboard/exports/mobile/invoice.blade.php
@@ -0,0 +1,192 @@
+<html>
+
+<head>
+  <meta charset="utf-8" />
+  <title>Tax Invoice</title>
+  <style>
+    * {
+      box-sizing: border-box;
+      font-family: "cairo", Times, serif;
+    }
+
+    .table-bordered td,
+    .table-bordered th {
+      border: 1px solid #ddd;
+      padding: 10px;
+      word-break: break-all;
+    }
+
+    body {
+      margin: 0;
+      padding: 0;
+      font-size: 16px;
+    }
+
+    .h4-14 h4 {
+      font-size: 12px;
+      margin-top: 0;
+      margin-bottom: 5px;
+    }
+
+    .img {
+      margin-left: "auto";
+      margin-top: "auto";
+      height: 30px;
+    }
+
+    pre,
+    p {
+      /* width: 99%; */
+      /* overflow: auto; */
+      /* bpicklist: 1px solid #aaa; */
+      padding: 0;
+      margin: 0;
+    }
+
+    table {
+      font-family: arial, sans-serif;
+      width: 100%;
+      border-collapse: collapse;
+      padding: 1px;
+    }
+
+    tr{
+      border-bottom: 1px solid gray;
+    }
+    .hm-p p {
+      text-align: left;
+      padding: 1px;
+      padding: 5px 4px;
+    }
+
+    td,
+    th {
+      text-align: left;
+      padding: 8px 6px;
+    }
+
+    .table-b td,
+    .table-b th {
+      border: 1px solid #ddd;
+    }
+
+    th {
+      /* background-color: #ddd; */
+      text-align: right;
+    }
+
+    .hm-p td,
+    .hm-p th {
+      padding: 3px 0px;
+    }
+
+    .cropped {
+      float: right;
+      margin-bottom: 20px;
+      height: 100px;
+      /* height of container */
+      overflow: hidden;
+    }
+
+    .cropped img {
+      width: 400px;
+      margin: 8px 0px 0px 80px;
+    }
+
+    .main-pd-wrapper {
+      box-shadow: 0 0 10px #ddd;
+      background-color: #fff;
+      border-radius: 10px;
+      padding: 15px;
+    }
+
+    .table-bordered td,
+    .table-bordered th {
+      border: 1px solid #ddd;
+      padding: 10px;
+      font-size: 14px;
+    }
+
+    .invoice-items {
+      font-size: 14px;
+      border-top: 1px dashed #ddd;
+    }
+
+    .invoice-items td {
+      padding: 14px 0;
+
+    }
+  </style>
+</head>
+
+<body dir="rtl">
+  <section class="main-pd-wrapper" style="width: 450px; margin: auto">
+    <div style="
+                  text-align: center;
+                  margin: auto;
+                  line-height: 1.5;
+                  font-size: 14px;
+                  color: #4a4a4a;
+                ">
+
+      <p style="font-weight: bold; color: blue; margin-top: 15px; font-size: 18px;">
+       تم التحويل بنجاح
+      </p>
+    </div>
+    <table style="width: 100%; table-layout: fixed;margin-top:30px;">
+      <thead>
+        <tr>
+          <th style="width: 220px;color:blue;">نوع العملية</th>
+          <th>تحويل ويسترن يونيون</th>
+        </tr>
+        <tr>
+          <th style="width: 220px;color:blue;">تاريخ العملية</th>
+          <th>10/03/2022</th>
+        </tr>
+        <tr>
+          <th style="width: 220px;color:blue;">رقم MTCN</th>
+          <th>#124567890</th>
+        </tr>
+        <tr>
+          <th style="width: 220px;color:blue;">الرقم المرجعي</th>
+          <th>#1256445654</th>
+        </tr>
+        <tr>
+          <th style="width: 220px;color:blue;">مبلغ التحويل</th>
+          <th>123 (USD)</th>
+        </tr>
+        <tr>
+          <th style="width: 220px;color:blue;">رسوم التحويل</th>
+          <th>567 ر.س</th>
+        </tr>
+        <tr>
+          <th style="width: 220px;color:blue;">سعر الصرف</th>
+          <th>5.25</th>
+        </tr>
+        <tr>
+          <th style="width: 220px;color:blue;">من حساب رقم</th>
+          <th>رصيد باي</th>
+        </tr>
+        <tr>
+          <th style="width: 220px;color:blue;">إجمالي المبلغ</th>
+          <th>5.23.25 ر.س</th>
+        </tr>
+        <tr>
+          <th style="width: 220px;color:blue;">اسم المستفيد</th>
+          <th>محمد عبد الله</th>
+        </tr>
+        <tr>
+          <th style="width: 220px;color:blue;">عنوان المستفيد</th>
+          <th>الرياض</th>
+        </tr>
+        <tr>
+          <th style="width: 220px;color:blue;">الغرض من الحوالة</th>
+          <th>دعم الأسرة </th>
+        </tr>
+      </thead>
+      <tbody></tbody>
+    </table>
+  </section>
+</body>
+
+</html>
