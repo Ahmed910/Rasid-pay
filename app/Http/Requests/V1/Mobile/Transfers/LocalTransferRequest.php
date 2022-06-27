cd0a35d427bc6753c20fec2475f624271e33ab34
Taha <taha.mohamed2016@gmail.com>
2022-06-27 13:46:40 +0200

    Change Namespace Transfers requests

--- app/Http/Requests/V1/Mobile/Transfers/LocalTransferRequest.php
+++ app/Http/Requests/V1/Mobile/Transfers/LocalTransferRequest.php
@@ -7,15 +7,6 @@ use App\Models\Transfer;
 
 class LocalTransferRequest extends ApiMasterRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return true;
-    }
 
     /**
      * Get the validation rules that apply to the request.
--- app/Http/Requests/V1/Mobile/Transfers/WalletTransferRequest.php
+++ app/Http/Requests/V1/Mobile/Transfers/WalletTransferRequest.php
@@ -11,16 +11,6 @@ class WalletTransferRequest extends ApiMasterRequest
     private $user_object;
     private $message;
     /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return true;
-    }
-
-    /**
      * Get the validation rules that apply to the request.
      *
      * @return array
