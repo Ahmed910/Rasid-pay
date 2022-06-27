a33760296b0a9e1f6479ae6014d123f6c134a02f
Nader Hany <98961513+naderhany96@users.noreply.github.com>
2022-06-27 14:48:20 +0200

    Nader (#1430)
    * create static pages models + migs

---
+++ app/Models/StaticPage/StaticPage.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models\StaticPage;
+
+use App\Contracts\HasAssetsInterface;
+use App\Traits\HasAssetsTrait;
+use App\Traits\Loggable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Traits\Uuid;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Astrotomic\Translatable\Contracts\Translatable as TranslatableContract;
+use Astrotomic\Translatable\Translatable;
+
+class StaticPage extends Model implements TranslatableContract, HasAssetsInterface
+{
+    use HasFactory, Uuid, HasAssetsTrait, Translatable, SoftDeletes, Loggable;
+
+
+    #region properties
+    protected $appends = ['image'];
+    protected $guarded = ['created_at', 'deleted_at'];
+    public $translatedAttributes = ['name', 'description'];
+    public $assets = ["image"];
+    #endregion properties
+
+    public static function boot()
+    {
+        parent::boot();
+        static::saved(function ($model) {
+            $model->saveAssets($model, request());
+        });
+    }
+    #region mutators
+    #endregion mutators
+
+    #region scopes
+    #endregion scopes
+
+    #region relationships
+    #endregion relationships
+
+    #region custom Methods
+    #endregion custom Methods
+}
