4a1974343eb5c0d216014a539815a820ed2b8d2e
Mohamed Fathi <mohamedkdr66@gmail.com>
2022-06-28 10:22:30 +0200

    Revert "Merge pull request #1437 from Fintech-sa/fathi"
    This reverts commit f688b727c7804cd0260379042998e913b9065200, reversing
    changes made to 15aaa0a1acbb3d8193fde2b99f44fb1ab47dff3f.

--- resources/lang/ar/auth.php
+++ resources/lang/ar/auth.php
@@ -1,5 +1,70 @@
 <?php
 
-use Illuminate\Support\Arr;
+return [
 
-return Arr::undot(db_translations('ar', 'auth')->pluck('value', 'key'));
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'محاولة غير صالحة لتسجيل الدخول',
+    'password' => 'كلمة المرور  غير صحيحة',
+    'throttle' => 'تخطي عدد مرات الارسال رجاء الانتظار :seconds ثواني.',
+    'success_login' => 'تسجيل الدخول الى النظام برقم مستخدم :user',
+    'unauth' => 'قم بتسجيل الدخول أولا',
+    'logout_waiting_u_another_time' => 'تم تسجيل الخروج بنجاح, في انتظارك مرة أخرى',
+    'ban_permanent' => 'تم تعطيل الحساب',
+    'ban_temporary' => 'تم تعطيل الحساب لفترة من :ban_from  إلي :ban_to',
+    'success_send_login_code' => 'تم ارسال كود التحقق الى رقم الجوال',
+    'account_not_exists' => 'الحساب غير موجود',
+    'success_change_password' => 'تم تغيير كلمة المرور بنجاح',
+    'account_is_true' => 'البيانات المدخلة صحيحة',
+    'success_code_plz_add_new_password' => 'تم التحقق من كود الاسترجاع برجاء اضافة كلمة مرور جديدة',
+    'code_not_true' => 'الكود غير صحيح',
+    'login_title' => 'تسجيل الدخول',
+    'login_subtitle' => 'من فضلك أدخل رقم المستخدم وكلمة المرور',
+    'reset_password' => 'إعادة تعيين كلمة المرور',
+    'userID' => 'رقم المستخدم',
+    'your_userID' => 'أدخل الرقم',
+    'password' => 'أدخل كلمة المرور',
+    'remember' => 'تذكرني',
+    'reset_subtitle' => 'من فضلك أدخل بريدك الإلكتروني أو رقم جوالك لإرسال رمز
+            التحقق',
+    'user_banned' => 'هذا الحساب معطل',
+    'success_activate_notifcation' => 'تم تعديل الاشعارات بنجاح',
+    'success_signup' => 'تم إنشاء الحساب بنجاح يرجي التحقق من رقم الجوال',
+    'verify_phone' => 'برجاء تأكيد رقم الجوال',
+    'success_verify_phone' => 'تم تأكيد رقم الجوال بنجاح',
+    'fail_send' => 'فشل عملية الارسال',
+    'success_update_password' => 'تم تعديل كلمة المرور بنجاح',
+    'wrong_old_password' => 'كلمة المرور الحالية غير متطابقة',
+    'verify_phone_first' => 'يجب تأكيد رقم الهاتف أولا',
+    'success_verify_phone_make_login' => 'تم إنشاء الحساب بنجاح',
+    'success_login_mobile' => 'تسجيل دخول إلى التطبيق برقم هوية :user',
+    'success_update_verify_phone' => 'تم تحديث بياناتك بنجاح رجاء تأكيد رقم الجوال الجديد',
+    'password_used_before' => 'كلمة المرور مستخدمة من قبل',
+    '' => '',
+    '' => '',
+    '' => '',
+    '' => '',
+    '' => '',
+    '' => '',
+    '' => '',
+    '' => '',
+    '' => '',
+    '' => '',
+    '' => '',
+    '' => '',
+    '' => '',
+    '' => '',
+    '' => '',
+    '' => '',
+    '' => '',
+    '' => ''
+];
