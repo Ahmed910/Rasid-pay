db4a4f758b14f69b0c5f661f572ab647716b5c23
Mohamed Fathi <mohamedkdr66@gmail.com>
2022-06-28 11:36:07 +0200

    Revert "Revert "Revert "Merge pull request #1437 from Fintech-sa/fathi"""
    This reverts commit 823a4ce648b87d07a280d4c8f3d13e4b5f49f7a3.

--- resources/lang/ar/validation.php
+++ resources/lang/ar/validation.php
@@ -1,5 +1,303 @@
 <?php
 
-use Illuminate\Support\Arr;
+$attributes = [
+    'name'                  => 'حقل الاسم',
+    'fullname'                  => 'حقل الاسم',
+    'username'              => ' رقم المستخدم',
+    'login_id'              => ' رقم المستخدم',
+    'email'                 => ' البريد الالكتروني',
+    'first_name'            => 'حقل الاسم الأول',
+    'identity_number'            => 'حقل رقم الهوية / الإقامة',
+    'last_name'             => 'حقل اسم العائلة',
+    'password'              => 'حقل كلمة المرور',
+    'password_confirmation' => 'حقل تأكيد كلمة المرور',
+    'city_id'                  => 'حقل المدينة',
+    'country_id'               => 'حقل الدولة',
+    'address'               => 'حقل العنوان',
+    'phone'                 => 'حقل الهاتف',
+    'full_phone'            => 'حقل الهاتف',
+    'mobile'                => 'حقل الجوال',
+    'age'                   => 'حقل العمر',
+    'sex'                   => 'حقل الجنس',
+    'gender'                => 'حقل النوع',
+    'day'                   => 'حقل اليوم',
+    'month'                 => 'حقل الشهر',
+    'year'                  => 'حقل السنة',
+    'hour'                  => 'حقل ساعة',
+    'minute'                => 'حقل دقيقة',
+    'second'                => 'حقل ثانية',
+    'title'                 => 'حقل العنوان',
+    'content'               => 'حقل المُحتوى',
+    'description'           => 'حقل الوصف',
+    'desc'           => 'حقل الوصف',
+    'excerpt'               => 'حقل المُلخص',
+    'date'                  => 'حقل التاريخ',
+    'time'                  => 'حقل الوقت',
+    'available'             => 'حقل مُتاح',
+    'size'                  => 'حقل الحجم',
+    'transfer_purpose_id '  => 'حقل غرض التحويل',
+    'beneficiary_id '  => 'حقل المستفيد',
+    'wallet_bin'                  => 'حقل رقم المحفظة',
+    'ban_reason'                  => 'حقل سبب الحظر',
+    'is_ban'                  => 'حقل الحالة من الحظر',
+    'is_active'                  => ' الحالة مطلوبة ',
+    'reset_code'                  => 'رمز التحقق',
+    'department_id'                  => 'القسم ',
+    'reasonAction'                  => ' السبب',
+    'ban_from'                  => 'حقل تاريخ من',
+    'ban_to'                  => 'حقل تاريخ إلى',
+    'employee_id'       =>       'حقل اسم الموظف',
+    'current_password'       =>       'حقل كلمة المرور الحالية',
+    'new_password'       =>       'حقل كلمة المرور الجديدة',
+    'card_type'       =>       'حقل نوع البطاقة',
+    'charge_type'       =>       'حقل طريقة الشحن',
+    'contact_type'       =>       'حقل نوع الرسالة',
+    'card_id'       =>       'حقل البطاقة',
+    'wallet_transfer_method'       => 'حقل طريقة التحويل',
+    'transfer_method_value'       => 'حقل قيمة طريقة التحويل',
 
-return Arr::undot(db_translations('ar', 'validation')->pluck('value', 'key'));
+    //card package create screen
+    'client_id'          => 'اسم العميل',
+    'basic_discount'     => 'نسبة خصم البطاقة الأساسية',
+    'golden_discount'    => 'نسبة خصم البطاقة الذهبية',
+    'platinum_discount'  => 'نسبة خصم البطاقة البلاتينية',
+
+    'parent_id'   => 'حقل القسم الرئيسي',
+    'permission_list'   => 'حقل الصلاحيات  النظام مطلوب',
+    'permission_list.*'   => 'حقل الصلاحيات الفردية',
+    'group_list'   => 'حقل المجموعات',
+    'group_list.*'   => 'حقل المجموعات',
+    'banks.*.id' => 'حقل ID للبنك',
+    'banks.*.name' => 'حقل اسم البنك',
+    'banks.*.type' => 'حقل نوع البنك',
+    'banks.*.code' => 'حقل كود البنك',
+    'banks.*.site' => 'حقل موقع البنك',
+    'banks.*.transfer_amount' => 'حقل قيمة تكلفة التحويل',
+    'banks.*.commercial_record' => 'حقل السجل التجاري',
+    'banks.*.tax_number' => 'حقل الرقم الضريبي',
+    'banks.*.service_customer' => 'حقل خدمة العملاء',
+    'banks.*.is_active' => 'حقل is_active',
+    'banks.*.ar.name'   => 'حقل اسم الفرع',
+    'rasid_job_id' => ' اسم الوظيفة',
+    'branches.*.type' => 'حقل نوع البنك ',
+    'branches.*.code' => 'حقل كود البنك ',
+    'branches.*.site' => 'حقل موقع البنك ',
+    'branches.*.transfer_amount' => 'حقل القيمة المحولة  ',
+    'branches.*.commercial_record' => 'حقل السجل التجاري ',
+    'branches.*.tax_number' => 'حقل الرقم الضريبي ',
+    'branches.*.service_customer' => 'حقل خدمة العملاء  ',
+
+    // Rasid Pay
+    'parent_id'   => 'حقل القسم الرئيسي',
+    'permission_list'   => 'حقل الصلاحيات الفردية',
+    'permission_list.*'   => 'حقل الصلاحيات الفردية',
+    'group_list'   => 'حقل المجموعات',
+    'group_list.*'   => 'حقل المجموعات',
+    'code'    => 'رمز التحقق',
+    'iban_number' => 'حقل رقم الحساب ',
+    'relation' => 'حقل علاقة القرابة',
+    'recieve_option_id' => 'حقل  خيارات الاستلام',
+    'nationality' => 'حقل الجنسية',
+    'date_of_birth' => 'حقل تاريخ الميلاد',
+    'is_card_saved' => 'الاحتفاظ ببيانات البطاقة',
+    'amount' => 'المبلغ',
+    'owner_name' => 'الاسم على البطاقة',
+    'card_name' => 'اسم البطاقة',
+    'card_number' => 'الرقم على البطاقة',
+    'expire_at' => 'تاريخ إنتهاء البطاقة',
+    'today' => 'تاريخ غير صالح',
+    'discounts.0.package_discount' => 'حقل نسبة خصم البطاقة الأساسية ',
+    'discounts.1.package_discount' => 'حقل نسبة خصم البطاقة الذهبية',
+    'discounts.2.package_discount' => 'حقل نسبة خصم البطاقة البلاتينية',
+];
+
+foreach (config('translatable.locales') as $locale) {
+    $attributes["branches.*.$locale.name"] = "حقل " . trans('dashboard.attributes.name', [], $locale);
+    $attributes[$locale . '.name'] = "حقل " . trans('dashboard.attributes.name', [], $locale);
+    $attributes[$locale . '.description'] = "حقل " . trans('dashboard.attributes.desc', [], $locale);
+    $attributes[$locale . '.title'] = "حقل " . trans('dashboard.attributes.title');
+    $attributes[$locale . '.nationality'] = "حقل " . trans('dashboard.attributes.nationality', [], $locale);
+}
+
+return [
+    'accepted'             => 'يجب قبول :attribute.',
+    'accepted_if'          => 'يجب قبول :attribute في حالة :other يساوي :value.',
+    'active_url'           => ' :attribute لا يُمثّل رابطًا صحيحًا.',
+    'after'                => ':attribute غير صالح',
+    'after_or_equal'       => ' :attribute يجب أن يكون تاريخاً لاحقاً أو مطابقاً للتاريخ :date.',
+    'alpha'                => 'يجب أن لا يحتوي  :attribute سوى على حروف.',
+    'alpha_dash'           => 'يجب أن لا يحتوي  :attribute سوى على حروف، أرقام ومطّات.',
+    'alpha_num'            => 'يجب أن يحتوي  :attribute على حروفٍ وأرقامٍ فقط.',
+    'array'                => 'يجب أن يكون  :attribute ًمصفوفة.',
+    'before'               => 'يجب على  :attribute أن يكون تاريخًا سابقًا للتاريخ :date.',
+    'before_or_equal'      => ' :attribute يجب أن يكون تاريخا سابقا أو مطابقا لتاريخ :date.',
+    'between'              => [
+        'array'   => 'يجب أن يحتوي  :attribute على عدد من العناصر بين :min و :max.',
+        'file'    => 'يجب أن يكون حجم ملف  :attribute بين :min و :max كيلوبايت.',
+        'numeric' => 'يجب أن تكون قيمة  :attribute بين :min و :max.',
+        'string'  => 'يجب أن يكون عدد حروف نّص  :attribute بين :min و :max.',
+    ],
+    'boolean'              => 'يجب أن تكون قيمة  :attribute إما true أو false .',
+    'confirmed'            => ' التأكيد غير مُطابق لل :attribute.',
+    'current_password'     => 'كلمة المرور غير صحيحة.',
+    'date'                 => ' :attribute ليس تاريخًا صحيحًا.',
+    'date_equals'          => 'يجب أن يكون  :attribute مطابقاً للتاريخ :date.',
+    'date_format'          => 'لا يتوافق  :attribute مع الشكل :format.',
+    'declined'             => 'يجب رفض :attribute.',
+    'declined_if'          => 'يجب رفض :attribute عندما يكون :other بقيمة :value.',
+    'different'            => 'يجب أن يكون :attribute و :other مُختلفين.',
+    'digits'               => 'يجب أن يحتوي  :attribute على :digits رقمًا.',
+    'digits_between'       => 'يجب أن يحتوي  :attribute بين :min و :max رقمًا.',
+    'dimensions'           => 'ال:attribute يحتوي على أبعاد صورة غير صالحة.',
+    'distinct'             => 'لل :attribute قيمة مُكرّرة.',
+    'email'                => 'صيغة  :attribute غير صحيحة',
+    'ends_with'            => 'يجب أن ينتهي  :attribute بأحد القيم التالية: :values',
+    'enum'                 => ' :attribute المختار غير صالح.',
+    'exists'               => 'القيمة المحددة ل:attribute غير موجودة.',
+    'file'                 => 'ال :attribute يجب أن يكون ملفا.',
+    'filled'               => ' :attribute إجباري.',
+    'gt'                   => [
+        'array'   => 'يجب أن يحتوي  :attribute على أكثر من :value عناصر/عنصر.',
+        'file'    => 'يجب أن يكون حجم ملف  :attribute أكبر من :value كيلوبايت.',
+        'numeric' => 'يجب أن تكون قيمة  :attribute أكبر من :value.',
+        'string'  => 'يجب أن يكون طول نّص  :attribute أكثر من :value حروفٍ/حرفًا.',
+    ],
+    'gte'                  => [
+        'array'   => 'يجب أن يحتوي  :attribute على الأقل على :value عُنصرًا/عناصر.',
+        'file'    => 'يجب أن يكون حجم ملف  :attribute على الأقل :value كيلوبايت.',
+        'numeric' => 'يجب أن تكون قيمة  :attribute مساوية أو أكبر من :value.',
+        'string'  => 'يجب أن يكون طول نص  :attribute على الأقل :value حروفٍ/حرفًا.',
+    ],
+    'image'                => 'يجب أن يكون  :attribute صورةً.',
+    'in'                   => ' :attribute يجب ان يكون ضمن القيم الآتية: :values.',
+    'in_array'             => ' :attribute غير موجود في :other.',
+    'integer'              => 'يجب أن يكون  :attribute عددًا صحيحًا.',
+    'ip'                   => 'يجب أن يكون  :attribute عنوان IP صحيحًا.',
+    'ipv4'                 => 'يجب أن يكون  :attribute عنوان IPv4 صحيحًا.',
+    'ipv6'                 => 'يجب أن يكون  :attribute عنوان IPv6 صحيحًا.',
+    'json'                 => 'يجب أن يكون  :attribute نصًا من نوع JSON.',
+    'lt'                   => [
+        'array'   => 'يجب أن يحتوي  :attribute على أقل من :value عناصر/عنصر.',
+        'file'    => 'يجب أن يكون حجم ملف  :attribute أصغر من :value كيلوبايت.',
+        'numeric' => 'يجب أن تكون قيمة  :attribute أصغر من :value.',
+        'string'  => 'يجب أن يكون طول نّص  :attribute أقل من :value حروفٍ/حرفًا.',
+    ],
+    'lte'                  => [
+        'array'   => 'يجب أن لا يحتوي  :attribute على أكثر من :value عناصر/عنصر.',
+        'file'    => 'يجب أن لا يتجاوز حجم ملف  :attribute :value كيلوبايت.',
+        'numeric' => 'يجب أن تكون قيمة  :attribute مساوية أو أصغر من :value.',
+        'string'  => 'يجب أن لا يتجاوز طول نّص  :attribute :value حروفٍ/حرفًا.',
+    ],
+    'mac_address'          => 'ال :attribute يجب أن يكون عنوان MAC صالحاً.',
+    'max'                  => [
+        'array'   => 'يجب أن لا يحتوي  :attribute على أكثر من :max عناصر/عنصر.',
+        'file'    => 'يجب أن لا يتجاوز حجم ملف  :attribute :max كيلوبايت.',
+        'numeric' => 'يجب أن تكون قيمة  :attribute مساوية أو أصغر من :max.',
+        'string'  => 'يجب أن لا يتجاوز طول نّص  :attribute :max حروفٍ/حرفًا.',
+    ],
+    'mimes'                => 'يجب أن يكون ملفًا من نوع : :values.',
+    'mimetypes'            => 'يجب أن يكون ملفًا من نوع : :values.',
+    'min'                  => [
+        'array'   => 'يجب أن يحتوي  :attribute على الأقل على :min عُنصرًا/عناصر.',
+        'file'    => 'يجب أن يكون حجم ملف  :attribute على الأقل :min كيلوبايت.',
+        'numeric' => 'يجب أن تكون قيمة  :attribute مساوية أو أكبر من :min.',
+        'string'  => 'يجب أن يكون طول نص  :attribute على الأقل :min حروفٍ/حرفًا.',
+    ],
+    'multiple_of'          => ' :attribute يجب أن يكون من مضاعفات :value',
+    'not_in'               => 'عنصر ال :attribute غير صحيح.',
+    'not_regex'            => 'صيغة  :attribute غير صحيحة.',
+    'numeric'              => 'يجب على  :attribute أن يكون رقمًا.',
+    'password'             => 'كلمة المرور غير صحيحة.',
+    'present'              => 'يجب تقديم  :attribute.',
+    'prohibited'           => ' :attribute محظور.',
+    'prohibited_if'        => ' :attribute محظور إذا كان :other هو :value.',
+    'prohibited_unless'    => ' :attribute محظور ما لم يكن :other ضمن :values.',
+    'prohibits'            => 'ال :attribute يحظر تواجد ال :other.',
+    'regex'                => 'صيغة :attribute غير صحيحة',
+    'required'             => ' :attribute مطلوب.',
+    'required_array_keys'  => 'ال :attribute يجب أن يحتوي على مدخلات لـ: :values.',
+    'required_if'          => ' :attribute مطلوب في حال ما إذا كان :other يساوي :value.',
+    'required_unless'      => ' :attribute مطلوب في حال ما لم يكن :other يساوي :values.',
+    'required_with'        => ' :attribute مطلوب إذا توفّر :values.',
+    'required_with_all'    => ' :attribute مطلوب إذا توفّر :values.',
+    'required_without'     => ' :attribute مطلوب إذا لم يتوفّر :values.',
+    'required_without_all' => ' :attribute مطلوب إذا لم يتوفّر :values.',
+    'same'                 => 'يجب أن يتطابق  :attribute مع :other.',
+    'size'                 => [
+        'array'   => 'يجب أن يحتوي  :attribute على :size عنصرٍ/عناصر بالضبط.',
+        'file'    => 'يجب أن يكون حجم ملف  :attribute :size كيلوبايت.',
+        'numeric' => 'يجب أن تكون قيمة  :attribute مساوية لـ :size.',
+        'string'  => 'يجب أن يحتوي نص  :attribute على :size حروفٍ/حرفًا بالضبط.',
+    ],
+    'starts_with'          => 'يجب أن يبدأ  :attribute بأحد القيم التالية: :values',
+    'string'               => 'يجب أن يكون  :attribute نصًا.',
+    'timezone'             => 'يجب أن يكون  :attribute نطاقًا زمنيًا صحيحًا.',
+    'unique'               => 'قيمة  :attribute موجودة من قبل.',
+    'uploaded'             => 'فشل في تحميل الـ :attribute.',
+    'url'                  => 'صيغة رابط  :attribute غير صحيحة.',
+    'uuid'                 => ' :attribute يجب أن يكون بصيغة UUID سليمة.',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Custom Validation Attributes
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used to swap our attribute placeholder
+    | with something more reader friendly such as "E-Mail Address" instead
+    | of "email". This simply helps us make our message more expressive.
+    |
+    */
+
+
+    'attributes' => $attributes,
+
+    'custom' => [
+        'unique' => 'الاسم موجود من قبل',
+        'email' => [
+            'exists' => 'هذا البريد غير مسجل بالنظام',
+        ],
+        'phone' => [
+            'exists' => ' رقم الجوال غير مسجل بالنظام',
+        ],
+        'identity_number'=>[
+            'regex'  => 'رقم الهوية/ الاقامة  يجب ان لا يبدا بصفر'
+        ],
+    ],
+    'values' => [
+        'code_type' => [
+            'reset_code' => 'كود استعادة',
+            'verified_code' => 'كود تفعيل',
+            'login_code' => 'كود الدخول',
+        ],
+        'card_type' => [
+            'visa' => 'فيزا',
+            'mastercard' => 'ماستر كارد',
+            'american_express' => 'امريكان اكسبريس',
+        ],
+    ],
+
+    'department' => [
+        'required' => 'اسم القسم مطلوب',
+    ],
+
+    'job' => [
+        'required' => 'اسم الوظيفة مطلوب',
+    ],
+
+
+    'admin' => [
+        'required_name' => 'اسم المستخدم مطلوب',
+        'required_job' => 'الوظيفة مطلوبة',
+        'required_password' => 'كلمة المرور مطلوبة',
+        'unique_login_id' => 'رقم المستخدم موجود من قبل',
+        'unique_phone' => 'رقم الجوال موجود من قبل',
+        'unique_email' => 'البريد الإلكتروني موجود من قبل',
+        'confirmed_password' => 'كلمة المرور غير متطابقة',
+    ],
+
+    'client_package' => [
+
+        'gold_gt_basic' => 'يرجى ادخال نسبة اكبر من نسبة البطاقة الاساسية',
+        'platinum_gt_golden' => 'يرجى ادخال نسبة اكبر من نسبة البطاقة الذهبية',
+    ]
+];
