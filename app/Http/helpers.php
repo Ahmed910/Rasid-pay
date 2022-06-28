d93d8bb5351bb710fd5fcd3dcf8a6358239714a0
Mohamed Fathi <mohamedkdr66@gmail.com>
2022-06-28 09:13:13 +0200

    Fix Localizations trans

--- app/Http/helpers.php
+++ app/Http/helpers.php
@@ -12,7 +12,6 @@ if (!function_exists('check_phone_valid')) {
     {
         return preg_match('/^(:?(\+)|(00))?(:?966)?+(5|05)([503649187])([0-9]{7})$/', $number);
     }
-
 }
 
 if (!function_exists('check_password_vaild')) {
@@ -20,54 +19,50 @@ if (!function_exists('check_password_vaild')) {
     {
         return preg_match('/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{6,}$/', $password);
     }
-
-
 }
 
 if (!function_exists('check_iban_valid')) {
     function check_iban_valid($iban, $country = null)
     {
-        $iban = strtolower(str_replace(' ','',$iban));
-        $Countries =[
-            'al'=>28,'ad'=>24,'at'=>20,'az'=>28,'bh'=>22,'be'=>16,
-            'ba'=>20,'br'=>29,'bg'=>22,'cr'=>21,'hr'=>21,'cy'=>28,
-            'cz'=>24,'dk'=>18,'do'=>28,'ee'=>20,'fo'=>18,'fi'=>18,
-            'fr'=>27,'ge'=>22,'de'=>22,'gi'=>23,'gr'=>27,'gl'=>18,
-            'gt'=>28,'hu'=>28,'is'=>26,'ie'=>22,'il'=>23,'it'=>27,
-            'jo'=>30,'kz'=>20,'kw'=>30,'lv'=>21,'lb'=>28,'li'=>21,
-            'lt'=>20,'lu'=>20,'mk'=>19,'mt'=>31,'mr'=>27,'mu'=>30,
-            'mc'=>27,'md'=>24,'me'=>22,'nl'=>18,'no'=>15,'pk'=>24,
-            'ps'=>29,'pl'=>28,'pt'=>25,'qa'=>29,'ro'=>24,'sm'=>27,
-            'sa'=>24,'rs'=>22,'sk'=>24,'si'=>19,'es'=>24,'se'=>24,
-            'ch'=>21,'tn'=>24,'tr'=>26,'ae'=>23,'gb'=>22,'vg'=>24
+        $iban = strtolower(str_replace(' ', '', $iban));
+        $Countries = [
+            'al' => 28, 'ad' => 24, 'at' => 20, 'az' => 28, 'bh' => 22, 'be' => 16,
+            'ba' => 20, 'br' => 29, 'bg' => 22, 'cr' => 21, 'hr' => 21, 'cy' => 28,
+            'cz' => 24, 'dk' => 18, 'do' => 28, 'ee' => 20, 'fo' => 18, 'fi' => 18,
+            'fr' => 27, 'ge' => 22, 'de' => 22, 'gi' => 23, 'gr' => 27, 'gl' => 18,
+            'gt' => 28, 'hu' => 28, 'is' => 26, 'ie' => 22, 'il' => 23, 'it' => 27,
+            'jo' => 30, 'kz' => 20, 'kw' => 30, 'lv' => 21, 'lb' => 28, 'li' => 21,
+            'lt' => 20, 'lu' => 20, 'mk' => 19, 'mt' => 31, 'mr' => 27, 'mu' => 30,
+            'mc' => 27, 'md' => 24, 'me' => 22, 'nl' => 18, 'no' => 15, 'pk' => 24,
+            'ps' => 29, 'pl' => 28, 'pt' => 25, 'qa' => 29, 'ro' => 24, 'sm' => 27,
+            'sa' => 24, 'rs' => 22, 'sk' => 24, 'si' => 19, 'es' => 24, 'se' => 24,
+            'ch' => 21, 'tn' => 24, 'tr' => 26, 'ae' => 23, 'gb' => 22, 'vg' => 24
         ];
-        $Chars =[
-            'a'=>10,'b'=>11,'c'=>12,'d'=>13,'e'=>14,'f'=>15,'g'=>16,
-            'h'=>17,'i'=>18,'j'=>19,'k'=>20,'l'=>21,'m'=>22,'n'=>23,
-            'o'=>24,'p'=>25,'q'=>26,'r'=>27,'s'=>28,'t'=>29,'u'=>30,
-            'v'=>31,'w'=>32,'x'=>33,'y'=>34,'z'=>35
+        $Chars = [
+            'a' => 10, 'b' => 11, 'c' => 12, 'd' => 13, 'e' => 14, 'f' => 15, 'g' => 16,
+            'h' => 17, 'i' => 18, 'j' => 19, 'k' => 20, 'l' => 21, 'm' => 22, 'n' => 23,
+            'o' => 24, 'p' => 25, 'q' => 26, 'r' => 27, 's' => 28, 't' => 29, 'u' => 30,
+            'v' => 31, 'w' => 32, 'x' => 33, 'y' => 34, 'z' => 35
         ];
 
-        if(strlen($iban) == @$Countries[$country ?? substr($iban,0,2)]){
+        if (strlen($iban) == @$Countries[$country ?? substr($iban, 0, 2)]) {
 
-            $MovedChar = substr($iban, 4).substr($iban,0,4);
+            $MovedChar = substr($iban, 4) . substr($iban, 0, 4);
             $MovedCharArray = str_split($MovedChar);
             $NewString = "";
 
-            foreach($MovedCharArray AS $key => $value){
-                if(!is_numeric($MovedCharArray[$key])){
+            foreach ($MovedCharArray as $key => $value) {
+                if (!is_numeric($MovedCharArray[$key])) {
                     $MovedCharArray[$key] = $Chars[$MovedCharArray[$key]];
                 }
                 $NewString .= $MovedCharArray[$key];
             }
 
-            if(bcmod($NewString, '97') == 1)
-            {
+            if (bcmod($NewString, '97') == 1) {
                 return true;
             }
         }
         return false;
-
     }
 }
 
@@ -175,30 +170,27 @@ if (!function_exists('setting')) {
         }
         return;
     }
-}if (!function_exists('translations')) {
-    ;
-    function db_translations(string $local =null , $file=null)
+}
+
+if (!function_exists('db_translations')) {;
+    function db_translations(string $local = null, $file = null): Collection
     {
+        if (!$local) $local = app()->getLocale();
+
         if (Schema::hasTable('translations')) {
-            if (!$local) $local =app()->getLocale();
-                      $translation = (Cache::has('translations_'.$local)) ? Cache::get('translations_'.$local)
-                : Cache::rememberForever('translations_'.$local, function () use ($local,$file) {
-                if($file)  return \App\Models\Translation::where(['locale'=>$local,"file"=>$file])->get();
-                         return \App\Models\Translation::where('locale', $local)->get();
-            });
+            $translation = (Cache::has('translations_' . $local))
+                ? Cache::get('translations_' . $local)
+                : Cache::rememberForever('translations_' . $local, function () use ($local) {
+                    return \App\Models\Translation::where('locale', $local)->get();
+                });
 
-
-            if (is_string($translation)) {
-                (array)json_decode($translation);
-            }
-
+            if ($file)  return $translation->where('file', $file);
 
             return $translation;
         }
-        return;
+        return collect([]);
     }
 }
-
 
 if (!function_exists('countries_list')) {
     function countries_list($type = null, $locale = 'ar')
