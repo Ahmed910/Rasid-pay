aa8a0548d2e55c2237f56c2e8f438b56f4ff2f5e
Mohamed Fathi <mohamedkdr66@gmail.com>
2022-07-03 08:55:35 +0200

    Fix issue in check on file imageResource

--- app/Http/Resources/Dashboard/ImagesResource.php
+++ app/Http/Resources/Dashboard/ImagesResource.php
@@ -3,16 +3,20 @@
 namespace App\Http\Resources\Dashboard;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 class ImagesResource extends JsonResource
 {
     public function toArray($request)
     {
-        $filePath = url('') . $this->media;
+        $filePath = $this->media;
 
         return [
             "id"    => $this->id,
-            "media" => (file_exists($filePath)) ? $filePath : asset('dashboardAssets/images/brand/no-img.png'),
+            "media" => Storage::disk('public')->exists(Str::replace("/storage", "", $filePath))
+                ? url($filePath)
+                : asset('dashboardAssets/images/brand/no-img.png'),
             "name"  => $this->option,
         ];
     }
