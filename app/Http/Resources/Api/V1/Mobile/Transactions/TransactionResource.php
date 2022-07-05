525ab4b2c73caac4d6c9f1aadcc618fa8fced407
Mohamed Fathi <mohamedkdr66@gmail.com>
2022-07-05 13:07:12 +0200

    Fix issue in TransactionReource

--- app/Http/Resources/Api/V1/Mobile/Transactions/TransactionResource.php
+++ app/Http/Resources/Api/V1/Mobile/Transactions/TransactionResource.php
@@ -28,7 +28,7 @@ class TransactionResource extends JsonResource
                 'trans_status_translate' =>  trans("mobile.transaction.status_cases.{$this->trans_status}"),
                 'to_currency' =>  $this->transactionable?->bankTransfer?->toCurrency?->currency_code,
                 'exchange_rate' =>  (string) $this->transactionable?->bankTransfer?->exchange_rate,
-                'to_amount' =>  $this->amount * $this->transactionable?->bankTransfer?->exchange_rate,
+                'to_amount' =>  (string) ($this->amount * $this->transactionable?->bankTransfer?->exchange_rate),
                 'transfer_fees' =>  (string) $this->transactionable?->transfer_fees ?? 0,
                 'transfer_purpose' =>  $this->transactionable?->transferPurpose?->name,
                 'recieve_option' =>  $this->transactionable?->bankTransfer?->recieveOption?->name,
