4f9f189bc1c11d1bf8ad44d405e44b014fd41c03
Mohamed Fathi <mohamedkdr66@gmail.com>
2022-07-06 14:07:47 +0200

    Hot fix

--- database/migrations/2022_07_06_144643_add_message_type_id_to_contacts.php
+++ database/migrations/2022_07_06_144643_add_message_type_id_to_contacts.php
@@ -4,18 +4,18 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddMessageTypeIdToMessageTypes extends Migration
+class AddMessageTypeIdToContacts extends Migration
 {
     public function up()
     {
-        Schema::table('message_types', function (Blueprint $table) {
+        Schema::table('contacts', function (Blueprint $table) {
             $table->foreignUuid("message_type_id")->nullable()->constrained()->nullOnDelete();
         });
     }
 
     public function down()
     {
-        Schema::table('message_types', function (Blueprint $table) {
+        Schema::table('contacts', function (Blueprint $table) {
             $table->dropColumn('message_type_id');
         });
     }
