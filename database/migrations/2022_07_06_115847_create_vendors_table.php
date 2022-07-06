1a869d3d536a9803af76e99e63cefd6db52d4cca
Mohamed Fathi <mohamedkdr66@gmail.com>
2022-07-06 11:15:58 +0200

    Add migrations and models for vendors

---
+++ database/migrations/2022_07_06_115847_create_vendors_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\Models\Vendor\Vendor;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVendorsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('vendors', function (Blueprint $table) {
+            $table->uuid("id")->primary();
+            $table->enum('type', Vendor::TYPES);
+            $table->string('commercial_record');
+            $table->string('tax_number');
+            $table->string('iban');
+            $table->boolean('is_support_maak');
+            $table->boolean('is_active');
+            $table->foreignUuid("added_by_id")->nullable()->constrained('users')->nullOnDelete();
+
+            $table->timestamps();
+        });
+
+        Schema::create('vendor_translations', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->foreignUuid('vendor_id')->constrained('vendors')->onDelete('cascade');
+            $table->string('locale')->index();
+            $table->string('name', 100);
+
+            $table->unique(['vendor_id', 'locale']);
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('vendor_translations');
+        Schema::dropIfExists('vendors');
+    }
+}
