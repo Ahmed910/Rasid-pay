a82443c543949743822008aba42fcb15b1268495
eman rashad <emanrashad173.fintech@gmail.com>
2022-07-17 11:55:26 +0200

    fix message notification toggle

--- resources/lang/en/Temp/auth.php
+++ resources/lang/en/Temp/auth.php
@@ -7,5 +7,7 @@ return [
     "password" => "The provided password is incorrect.",
     "throttle" => "Too many login attempts. Please try again in :seconds seconds.",
     "failed" => "These credentials do not match our records.",
-    "login_title" => "login"
+    "login_title" => "login",
+    "success_activate_notifcation" => "Activate notifcation successfully",
+    "success_unactivate_notifcation" => "Unactivate notifcation successfully",
 ];
