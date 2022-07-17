de994d02726c41e5bb912c3f9c4d9121289b4389
Mohamed Fathi <mohamedkdr66@gmail.com>
2022-07-17 15:52:14 +0200

    Fix issue in messageType

--- app/Http/Resources/MessageTypeCollection.php
+++ app/Http/Resources/MessageTypeCollection.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Resources;
 
-use App\Http\Resources\Api\V1\Mobile\MessageTypeResource;
 use App\Http\Resources\Dashboard\ActivityLogResource;
+use App\Http\Resources\Dashboard\MessageTypeResource;
 use App\Models\MessageType\MessageType;
 use Illuminate\Http\Resources\Json\ResourceCollection;
 
