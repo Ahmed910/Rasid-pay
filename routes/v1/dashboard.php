e5955c5a8ce275350f3c1ac5e3b9443e8a41bc4c
naderhany96 <naderhany613@gmail.com>
2022-07-17 10:15:36 +0200

    add static pages without pagination endpoint

--- routes/v1/dashboard.php
+++ routes/v1/dashboard.php
@@ -53,6 +53,8 @@ Route::middleware('maintenance_mode')->group(function () {
         Route::get('all-employees/{department}', 'EmployeeController@getEmployeesByDepartment');
         Route::get('all-groups/{except_id?}', 'GroupController@getGroups');
         Route::get('all-jobs/{department}', 'RasidJobController@getVacantJobs');
+        Route::get('all_static_pages', 'StaticPageController@getAllStaticPages');
+
 
         Route::controller('ActivityController')->name('activity_logs.')->prefix('activity_logs')->group(function () {
             Route::get('employees', 'getEmployees')->name('employees');
