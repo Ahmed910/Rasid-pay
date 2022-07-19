56290b8ee492a5dbe73545a97826a1ddcde6c990
Mohamed Fathi <mohamedkdr66@gmail.com>
2022-07-19 15:42:30 +0200

    Add is_another to transfers

--- app/Http/Requests/V1/Mobile/Transfers/WalletTransferRequest.php
+++ app/Http/Requests/V1/Mobile/Transfers/WalletTransferRequest.php
@@ -4,6 +4,7 @@ namespace App\Http\Requests\V1\Mobile\Transfers;
 
 use App\Http\Requests\ApiMasterRequest;
 use App\Models\Transfer;
+use App\Models\TransferPurpose\TransferPurpose;
 use App\Models\User;
 
 class WalletTransferRequest extends ApiMasterRequest
@@ -22,13 +23,21 @@ class WalletTransferRequest extends ApiMasterRequest
         //         "transfer_status" => 'required|in:hold,transfered'
         //     ];
         // }
+
+        $transferPurpose = TransferPurpose::find($this->transfer_purpose_id);
+        $notes = 'nullable';
+
+        if ($transferPurpose->is_another) {
+            $notes = 'required|string|max:1000';
+        }
+
         return [
             'amount'  => 'required|numeric|gte:'. (setting('min_wallet_transfer_amount') ?? 10) . '|lte:' . (setting('max_wallet_transfer_amount') ?? 10000),
             "citizen_id" => 'nullable|exists:users,id,user_type,citizen,register_status,completed',
             "otp_code" => 'required|exists:citizen_wallets,wallet_bin,citizen_id,'.auth()->id(),
             "wallet_transfer_method" => 'required|in:' . join(",", Transfer::WALLET_TRANSFER_METHODS),
             'transfer_purpose_id' => 'nullable|exists:transfer_purposes,id',
-            'notes'   => 'nullable|required_without:transfer_purpose_id|max:1000',
+            'notes'   => $notes,
             "transfer_method_value" => ['required',function ($attribute, $value, $fail) {
                 if(!is_bool($this->message)){
                     $fail($this->message);
