74ad6b8162f4eb5c2fc16626e0a87ddd0b39b019
Mohamed Karem <karem.fintech@gmail.com>
2022-07-20 11:20:20 +0200

    edit vendor branches validation

--- app/Http/Requests/V1/Dashboard/VendorBranchRequest.php
+++ app/Http/Requests/V1/Dashboard/VendorBranchRequest.php
@@ -22,8 +22,8 @@ class VendorBranchRequest extends ApiMasterRequest
             'branch_image' =>'required|image|mimes:jpg,jpeg,png,gif,svg|max:5120',
             'email' => 'required|email|unique:vendor_branches,email,' . $this->vendor_branch?->id,
             'phone' => 'required|numeric|digits_between:5,20|starts_with:9665,05|unique:vendor_branches,phone,' . $this->vendor_branch?->id,
-            'lat' => 'required|numeric|between:4,20',
-            'lng' => 'required|numeric|between:4,20',
+            'lat' => 'required|numeric|size:10',
+            'lng' => 'required|numeric|size:10',
         ];
         if(isset($vendor_branch) && $vendor_branch)
         {
