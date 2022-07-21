99e3c37002c12cafe65ea28666d5f4428543cc3e
Mohamed Fathi <mohamedkdr66@gmail.com>
2022-07-21 13:58:02 +0200

    Fix issue in ourAppController

--- app/Http/Resources/Api/V1/Mobile/OurAppResource.php
+++ app/Http/Resources/Api/V1/Mobile/OurAppResource.php
@@ -4,17 +4,10 @@ namespace App\Http\Resources\Api\V1\Mobile;
 
 use App\Http\Resources\Dashboard\GlobalTransResource;
 use App\Http\Resources\Dashboard\ImagesResource;
-use App\Http\Resources\Dashboard\SimpleUserResource;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class OurAppResource extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @param \Illuminate\Http\Request $request
-     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
-     */
     public function toArray($request)
     {
         $locales = [];
@@ -23,14 +16,16 @@ class OurAppResource extends JsonResource
                 $locales['translations'][$locale] = GlobalTransResource::make($this->translations->firstWhere('locale', $locale));
             }
         }
-        return [
-                'id' => $this->id,
-                'name' => $this->name,
-                'description' => $this->description,
-                'is_active' => (bool)$this->is_active,
-                "images" => ImagesResource::collection($this->whenLoaded("images")),
-                'created_at' => $this->created_at,
-            ] + $locales;
 
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'description' => $this->description,
+            'is_active' => (bool) $this->is_active,
+            'android_link' => $this->android_link,
+            'ios_link' => $this->ios_link,
+            "images" => ImagesResource::collection($this->whenLoaded("images")),
+            'created_at' => $this->created_at,
+        ] + $locales;
     }
 }
