9b44f0013b110a7462ffc515037788801cf351b7
Mohamed Fathi <mohamedkdr66@gmail.com>
2022-07-21 13:56:25 +0200

    Fix issue in ourAppController

--- app/Http/Controllers/Api/V1/Mobile/OurAppController.php
+++ app/Http/Controllers/Api/V1/Mobile/OurAppController.php
@@ -9,66 +9,16 @@ use Illuminate\Http\Request;
 
 class OurAppController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index(Request  $request)
     {
         $ourApps = OurApp::search($request)
-            ->ListsTranslations('name','description')
-            ->sortBy($request)->get() ;
-
+            ->ListsTranslations('name', 'description')
+            ->ordeBy('order')
+            ->get();
 
         return OurAppResource::collection($ourApps)->additional([
             'status' => true,
             'message' => ''
         ]);
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
     }
 }
