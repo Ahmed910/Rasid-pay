4e4dfc0c385864e0c178ef9b8cce051127e11a63
naderhany96 <naderhany613@gmail.com>
2022-07-21 13:59:00 +0200

    edit our app in mobile

--- app/Http/Resources/Api/V1/Mobile/OurAppResource.php
+++ app/Http/Resources/Api/V1/Mobile/OurAppResource.php
@@ -2,9 +2,7 @@
 
 namespace App\Http\Resources\Api\V1\Mobile;
 
-use App\Http\Resources\Dashboard\GlobalTransResource;
 use App\Http\Resources\Dashboard\ImagesResource;
-use App\Http\Resources\Dashboard\SimpleUserResource;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class OurAppResource extends JsonResource
@@ -17,20 +15,14 @@ class OurAppResource extends JsonResource
      */
     public function toArray($request)
     {
-        $locales = [];
-        if ($this->relationLoaded('translations')) {
-            foreach (config('translatable.locales') as $locale) {
-                $locales['translations'][$locale] = GlobalTransResource::make($this->translations->firstWhere('locale', $locale));
-            }
-        }
+        
         return [
                 'id' => $this->id,
                 'name' => $this->name,
                 'description' => $this->description,
-                'is_active' => (bool)$this->is_active,
                 "images" => ImagesResource::collection($this->whenLoaded("images")),
                 'created_at' => $this->created_at,
-            ] + $locales;
+            ];
 
     }
 }
