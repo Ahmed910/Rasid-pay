092d946a62898a4e3bd1d65a72d7e0a37ab2f20b
Mohamed Karem <karem.fintech@gmail.com>
2022-07-25 17:03:40 +0200

    edit global transfer validation

--- app/Http/Requests/V1/Mobile/Transfers/GlobalTransferRequest.php
+++ app/Http/Requests/V1/Mobile/Transfers/GlobalTransferRequest.php
@@ -14,8 +14,8 @@ class GlobalTransferRequest extends ApiMasterRequest
             'amount' => 'required|numeric|gte:' . (setting('rasidpay_inttransfer_minvalue') ?? 10) . '|lte:' . (setting('rasidpay_inttransfer_maxvalue') ?? 10000),
             //            'amount_transfer'     => 'required|numeric',
             'transfer_purpose_id' => 'nullable|exists:transfer_purposes,id',
-            'currency_id' => 'required|exists:countries,id',
-            'to_currency_id' => 'required|exists:countries,id',
+            'currency_id' => 'required|exists:currencies,id',
+            'to_currency_id' => 'required|exists:currencies,id',
             'fee_upon' => 'required|in:' . join(',', Transfer::FEE_UPON),
             'beneficiary_id' => 'nullable|exists:beneficiaries,id',
             'notes' => 'nullable|required_without:transfer_purpose_id|max:1000'
