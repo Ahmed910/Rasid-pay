ea27bf33c4f51d14e509be4e2d35ca80c4780c5d
Mohamed Fathi <mohamedkdr66@gmail.com>
2022-07-26 10:25:42 +0200

    Add seed for currencies

---
+++ database/seeders/CountryTransSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Country\Country;
+use Illuminate\Database\Seeder;
+
+class CountryTransSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $trans = include database_path('Intial_data/countries_trans.php');
+
+        foreach ($trans as $single) {
+            $country = Country::firstWhere(['currency_code' => $single['key']]);
+
+            if ($country) {
+                if ($country->translate('ar')) {
+                    $country->translate('ar')->currency = $single['ar']['name'];
+                    $country->save();
+                }
+            }
+        }
+    }
+}
