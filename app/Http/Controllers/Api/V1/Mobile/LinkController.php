83cca8a7cdee3de7f73e84ece17c583a61763ce8
Mohamed Fathi <mohamedkdr66@gmail.com>
2022-07-26 16:25:41 +0200

    Links

--- app/Http/Controllers/Api/V1/Mobile/LinkController.php
+++ app/Http/Controllers/Api/V1/Mobile/LinkController.php
@@ -11,13 +11,15 @@ class LinkController extends Controller
     public function show($static_page_key)
     {
 
-        $static_page_data = \DB::table('static_pages')
-           ->join('links','static_pages.id','links.static_page_id')
-           ->join('static_page_translations','static_page_translations.static_page_id','static_pages.id')
-           ->where('links.key',$static_page_key)
-           ->select('static_pages.id','static_page_translations.name','static_page_translations.description')
-           ->get()->all();
+        $static_page_data = StaticPage::join('links', 'static_pages.id', 'links.static_page_id')
+            ->join('static_page_translations', 'static_page_translations.static_page_id', 'static_pages.id')
+            ->where('links.key', $static_page_key)
+            ->select('static_pages.id', 'static_page_translations.name', 'static_page_translations.description')
+            ->first();
 
-           return response()->json(['data' => $static_page_data + ['key' => $static_page_data],'status' => true,'message' =>'']);
+        return response()->json(['data' => [
+            'description' => $static_page_data->description,
+            'name' => $static_page_data->name
+        ], 'status' => true, 'message' => '']);
     }
 }
