7e550d06cfa4c69e765d1d8d8ea1da32b60df0c7
naderhany96 <naderhany613@gmail.com>
2022-07-26 14:42:10 +0200

    add validation to send message mobile

--- app/Http/Requests/V1/Mobile/ContactRequest.php
+++ app/Http/Requests/V1/Mobile/ContactRequest.php
@@ -14,11 +14,11 @@ class ContactRequest extends ApiMasterRequest
     public function rules()
     {
         return [
-            "fullname" => 'nullable|string|max:255',
-            'email' => 'nullable|email|max:225',
-            'phone' => ["nullable", "numeric", function ($attribute, $value, $fail) {
+            "fullname" => 'required|string|min:2|max:100',
+            'email' => 'required|email|regex:/(.+)@(.+)\.(.+)/i|max:225',
+            'phone' => ["required", "numeric", function ($attribute, $value, $fail) {
                 if (!check_phone_valid($value)) {
-                    $fail(trans('mobile.validation.invalid_phone'));
+                    $fail(trans('mobile.contacts.validation.phone.invalid'));
                 }
             }],
             "content" => 'required|string|min:10|max:500',
@@ -33,5 +33,21 @@ class ContactRequest extends ApiMasterRequest
         $this->merge([
             'phone' => @$data['phone'] ? filter_mobile_number($data['phone']) : @$data['phone']
         ]);
+    }
+
+    public function messages()
+    {
+        return [
+            'fullname.required'        =>  __('mobile.validation.fullname.required'),   
+            'fullname.min'             =>  __('mobile.validation.fullname.min'),
+            'email.required'           =>  __('mobile.validation.email.required'),
+            'email.email'              =>  __('mobile.validation.email.email'),
+            'email.regex'              =>  __('mobile.validation.email.regex'),
+            'phone.required'           =>  __('mobile.validation.phone.required'),
+            'content.required'         =>  __('mobile.contacts.validation.content.required'),
+            'content.min'              =>  __('mobile.contacts.validation.content.min'),
+            'message_type_id.required' =>  __('mobile.contacts.validation.message_type.required'),
+            'message_type_id.exists' =>  __('mobile.contacts.validation.message_type.exists'),
+        ];
     }
 }
