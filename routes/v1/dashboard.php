02423e6f0944e125ddb5d1ffdd55d0f18b04e75f
eman rashad <emanrashad173.fintech@gmail.com>
2022-07-27 14:26:04 +0200

    edit route

--- routes/v1/dashboard.php
+++ routes/v1/dashboard.php
@@ -176,7 +176,7 @@ Route::middleware('maintenance_mode')->group(function () {
             ]);
 
             Route::apiResource('vendor_branches', 'VendorBranchController')->except('get_vendors');
-            Route::apiResource('vendor_package', 'VendorPackageController')->except('destroy');
+            Route::apiResource('vendor_packages', 'VendorPackageController')->except('destroy');
             Route::apiResource('citizens', 'CitizenController')->only('index', 'show', 'update');
             Route::apiResource('settings', 'SettingController')->only(['index', 'store']);
             Route::apiResource('links', 'LinkController')->only(['index','update']);
