2ad2ebb3da5784abbb3f3f183c415e2048d8f9b2
Mohamed Fathi <mohamedkdr66@gmail.com>
2022-07-28 12:38:51 +0200

    Add citizen export

---
+++ app/Exports/CitizenExport.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Exports;
+
+use App\Models\Citizen;
+use Illuminate\Contracts\View\View;
+use Maatwebsite\Excel\Concerns\FromView;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+
+class CitizenExport implements FromView, ShouldAutoSize
+{
+    protected $request;
+
+    public function __construct($request)
+    {
+        $this->request = $request;
+    }
+
+    public function view(): View
+    {
+        $citizensQuery = Citizen::with(['user', "enabledPackage"])
+            ->whereHas('user', fn ($q) => $q->where('register_status', 'completed'))
+            ->CustomDateFromTo($this->request)
+            ->search($this->request)
+            ->sortBy($this->request)
+            ->get();
+
+        if (!$this->request->has('created_from')) {
+            $createdFrom = Citizen::selectRaw('MIN(created_at) as min_created_at')->value('min_created_at');
+        }
+
+        return view('dashboard.exports.citizen', [
+            'citizens' => $citizensQuery,
+            'date_from'   => format_date($this->request->created_from) ?? format_date($createdFrom),
+            'date_to'     => format_date($this->request->created_to) ?? format_date(now()),
+            'userId'      => auth()->user()->login_id,
+        ]);
+    }
+}
