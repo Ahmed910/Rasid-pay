db11157fc13d1bc3eec726400eee95c7a49b2c17
Mohamed Karem <karem.fintech@gmail.com>
2022-08-01 09:24:25 +0200

    edit export message type

--- resources/views/dashboard/exports/message_type.blade.php
+++ resources/views/dashboard/exports/message_type.blade.php
@@ -2,28 +2,28 @@
 
 @section('content')
 
-<table id="departmentTable" class="table">
-  <thead>
+  <table id="departmentTable" class="table">
+    <thead>
     @include('dashboard.exports.header',['topic'=>'أنواع الرسائل'])
     <tr>
       <th>#</th>
-      <th> الحالة </th>
       <th> الاسم</th>
       <th>عدد الموظفين</th>
+      <th> الحالة</th>
       <th>تاريخ الإضافة</th>
     </tr>
-  </thead>
-  <tbody>
+    </thead>
+    <tbody>
     @foreach ($messageTypes as $message_type)
-    <tr>
-      <td>{{ $loop->iteration }}</td>
-      <td>{{ (bool) $message_type->is_active ?? '' }}</td>
-      <td>{{ $message_type->name ?? '' }}</td>
-      <td>{{ $message_type->admins_count ?? '' }}</td>
-      <td>{{ $message_type->created_at ?? '' }}</td>
-    </tr>
+      <tr>
+        <td>{{ $loop->iteration }}</td>
+        <td>{{ $message_type->name ?? '' }}</td>
+        <td>{{ $message_type->admins_count ?? '' }}</td>
+        <td>{{ $message_type->is_active ?  trans('Temp.dashboard.general.active'): trans('temp.dashboard.general.inactive') }}</td>
+        <td>{{ $message_type->created_at ?? '' }}</td>
+      </tr>
     @endforeach
-  </tbody>
-</table>
+    </tbody>
+  </table>
 
 @endsection
