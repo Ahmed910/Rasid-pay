393d9de92f86b84d128f9c502d435cacf1cd11d2
Mohamed Karem <karem.fintech@gmail.com>
2022-08-01 13:58:08 +0200

    check on amount before transfer

--- app/Http/Controllers/Api/V1/Mobile/Transfers/TransferController.php
+++ app/Http/Controllers/Api/V1/Mobile/Transfers/TransferController.php
@@ -8,13 +8,14 @@ use App\Http\Resources\Api\V1\Mobile\TransferResource;
 use App\Models\CitizenWallet;
 use App\Models\Transaction;
 use App\Models\Transfer;
+use Illuminate\Http\Request;
 
 
 class TransferController extends Controller
 {
     public function index(TransferTypeRequest $request)
     {
-        $transfers = Transfer::where('from_user_id', auth()->id())->where('transfer_type','wallet')->with('fromUser')->latest()->paginate((int)($request->per_page ?? config("globals.per_page")));
+        $transfers = Transfer::where('from_user_id', auth()->id())->where('transfer_type', 'wallet')->with('fromUser')->latest()->paginate((int)($request->per_page ?? config("globals.per_page")));
 
         return TransferResource::collection($transfers)->additional(
             [
@@ -29,16 +30,16 @@ class TransferController extends Controller
     {
         $user_id = auth()->id();
 
-        $transfer = Transfer::where(['from_user_id'=>$user_id,'transfer_status'=>Transfer::PENDING,'transfer_type'=>Transfer::WALLET])->findOrFail($transfer_id);
-        $current_user_wallet = CitizenWallet::where('citizen_id',$transfer->from_user_id)->firstOrFail();
+        $transfer = Transfer::where(['from_user_id' => $user_id, 'transfer_status' => Transfer::PENDING, 'transfer_type' => Transfer::WALLET])->findOrFail($transfer_id);
+        $current_user_wallet = CitizenWallet::where('citizen_id', $transfer->from_user_id)->firstOrFail();
         $transfer->update(['transfer_status' => Transfer::CANCELED]);
         $citizen_wallet_data = ["cash_back" => \DB::raw('cash_back + ' . $transfer->cashback_amount), 'main_balance' => \DB::raw('main_balance + ' . $transfer->main_amount)];
         $current_user_wallet->update($citizen_wallet_data);
-       // $transfered_user_wallet->decrement('main_balance',$transfer->amount);
+        // $transfered_user_wallet->decrement('main_balance',$transfer->amount);
         $transfer->transaction()->update([
-            'trans_type'=> Transaction::TRANACTION_TYPES[0],
-            'trans_status'=> Transaction::CANCELED,
-            'amount'=> $transfer->main_amount,
+            'trans_type' => Transaction::TRANACTION_TYPES[0],
+            'trans_status' => Transaction::CANCELED,
+            'amount' => $transfer->main_amount,
         ]);
 
         return TransferResource::make($transfer->refresh())->additional([
@@ -58,5 +59,57 @@ class TransferController extends Controller
                 'status' => true,
                 'message' => __('dashboard.general.success_delete')
             ]);
+    }
+
+
+    public function checkTransferredAmount(Request $request)
+    {
+        $maxReceiverTransfer = setting('rasidpay_usertransfer_maxvalue_perreciever');
+        // charge transfer
+        if ($request->transfer_type == 'wallet_transfer') {
+            $rules = [
+                "amount" => 'gte:' . setting('rasidpay_wallettransfer_minvalue') . '|lte:' . setting('rasidpay_wallettransfer_maxvalue')
+            ];
+            $messages = [
+                'amount.gte' => trans('validation.wallet_transfer.amount.gte', ['min_amount' => (setting('rasidpay_wallettransfer_minvalue'))]),
+                'amount.lte' => trans('validation.wallet_transfer.amount.lte', ['max_amount' => (setting('rasidpay_wallettransfer_maxvalue'))]),
+            ];
+        }
+        // wallet charge
+        if ($request->transfer_type == 'wallet_charge') {
+            $rules = [
+                "amount" => 'gte:' . setting('rasidpay_walletcharge_minvalue') . '|lte:' . setting('rasidpay_walletcharge_maxvalue')
+            ];
+            $messages = [
+                'amount.gte' => trans('validation.wallet_charge.amount.gte', ['min_amount' => (setting('rasidpay_walletcharge_minvalue'))]),
+                'amount.lte' => trans('validation.wallet_charge.amount.lte', ['max_amount' => (setting('rasidpay_walletcharge_maxvalue'))]),
+            ];
+        }
+        // local transfer
+        if ($request->transfer_type == 'local_transfer') {
+            $rules = [
+                "amount" => 'gte:' . setting('rasidpay_localtransfer_minvalue') . '|lte:' . setting('rasidpay_localtransfer_maxvalue')
+            ];
+            $messages = [
+                'amount.gte' => trans('validation.local_transfers.amount.gte', ['min_amount' => (setting('rasidpay_localtransfer_minvalue'))]),
+                'amount.lte' => trans('validation.local_transfers.amount.lte', ['max_amount' => (setting('rasidpay_localtransfer_maxvalue'))]),
+            ];
+        }
+        // global transfer
+        if ($request->transfer_type == 'global_transfer') {
+            $rules = [
+                "amount" => 'gte:' . setting('rasidpay_globaltransfer_minvalue') . '|lte:' . setting('rasidpay_globaltransfer_maxvalue')
+            ];
+            $messages = [
+                'amount.gte' => trans('validation.global_transfers.amount.gte', ['min_amount' => (setting('rasidpay_globaltransfer_minvalue'))]),
+                'amount.lte' => trans('validation.global_transfers.amount.lte', ['max_amount' => (setting('rasidpay_globaltransfer_maxvalue'))]),
+            ];
+        }
+        $validator = \Validator::make($request->all(), $rules, $messages);
+        return response()->json([
+            'data' => null,
+            'message' => $validator->errors() ?? "",
+            'status' => false], 422);
+
     }
 }
