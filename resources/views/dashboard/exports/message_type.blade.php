05449e9909a842d13e5e6f0dcf52cae866444507
eman rashad <emanrashad173.fintech@gmail.com>
2022-08-01 10:18:47 +0200

    fix issue #290

--- resources/views/dashboard/exports/message_type.blade.php
+++ resources/views/dashboard/exports/message_type.blade.php
@@ -5,12 +5,18 @@
   <table id="departmentTable" class="table">
     <thead>
     @include('dashboard.exports.header',['topic'=>'أنواع الرسائل'])
-    <tr>
-      <th>#</th>
-      <th> الاسم</th>
-      <th>عدد الموظفين</th>
-      <th> الحالة</th>
-      <th>تاريخ الإضافة</th>
+
+
+      <tr>
+      <th class="border-bottom-0">#</th>
+      <th class="border-bottom-0">
+        @lang('dashboard.message_type.name')</th>
+      <th class="border-bottom-0">
+        @lang('dashboard.message_type.employee_count')</th>
+      <th class="border-bottom-0">
+        @lang('dashboard.general.status')</th>
+      <th class="border-bottom-0">
+       @lang('dashboard.general.created_at')</th>
     </tr>
     </thead>
     <tbody>
@@ -19,8 +25,21 @@
         <td>{{ $loop->iteration }}</td>
         <td>{{ $message_type->name ?? '' }}</td>
         <td>{{ $message_type->admins_count ?? '' }}</td>
-        <td>{{ $message_type->is_active ?  trans('Temp.dashboard.general.active'): trans('temp.dashboard.general.inactive') }}</td>
-        <td>{{ $message_type->created_at ?? '' }}</td>
+
+         <td>
+        @if($message_type->is_active)
+        <div class="active">
+          <i class="mdi mdi-check-circle-outline"></i>
+          {{ trans('dashboard.message_type.active_cases.1') }}
+        </div>
+        @else
+        <div class="unactive">
+          <i class="mdi mdi-cancel"></i>
+          {{ trans('dashboard.message_type.active_cases.0') }}
+        </div>
+        @endif
+      </td>
+      <td>{{ $message_type->created_at ?? '' }}</td>
       </tr>
     @endforeach
     </tbody>
