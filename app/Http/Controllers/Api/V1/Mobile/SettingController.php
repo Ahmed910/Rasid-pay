7a855821ccb72f9fac40f29bc1bcb2494ba36700
Mohamed Fathi <mohamedkdr66@gmail.com>
2022-08-02 14:06:42 +0200

    Add settings endpoint

---
+++ app/Http/Controllers/Api/V1/Mobile/SettingController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1\Mobile;
+
+use App\Http\Controllers\Controller;
+
+class SettingController extends Controller
+{
+    public function index()
+    {
+        return response()->json([
+            'data' => [
+                'local_transfer_fees' => setting('rasidpay_localtransfer_transferfees')
+            ],
+            'status' => true,
+            'message' =>  trans('dashboard.general.sent_successfully'),
+        ]);
+    }
+}
