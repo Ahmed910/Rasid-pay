d698bbea532f258e36640ab315e9c760b9a1c278
Mohamed Karem <karem.fintech@gmail.com>
2022-08-03 10:10:41 +0200

    edit transfersa invoice

--- app/Http/Controllers/Api/V1/Mobile/SettingController.php
+++ app/Http/Controllers/Api/V1/Mobile/SettingController.php
@@ -3,17 +3,20 @@
 namespace App\Http\Controllers\Api\V1\Mobile;
 
 use App\Http\Controllers\Controller;
+use App\Models\TransferFee;
 
 class SettingController extends Controller
 {
     public function index()
     {
+        $global_fees = TransferFee::select('amount_from', 'amount_to', 'amount_fee')->get();
         return response()->json([
             'data' => [
-                'local_transfer_fees' => setting('rasidpay_localtransfer_transferfees')
+                'local_transfer_fees' => setting('rasidpay_localtransfer_transferfees'),
+                'ranges' => $global_fees,
             ],
             'status' => true,
-            'message' =>  '',
+            'message' => '',
         ]);
     }
 }
