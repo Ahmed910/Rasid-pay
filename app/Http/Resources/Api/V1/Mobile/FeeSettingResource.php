cd8d0b64ea9b008e01fca982d37579dbc3b4f902
Mohamed Karem <karem.fintech@gmail.com>
2022-08-04 12:59:38 +0200

    edit transfer fees for global transfers

---
+++ app/Http/Resources/Api/V1/Mobile/FeeSettingResource.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Resources\Api\V1\Mobile;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class FeeSettingResource extends JsonResource
+{
+    public function toArray($request)
+    {
+
+        return [
+            'amount_from' => number_format($this->amount_from, 2, '.', ''),
+            'amount_to' => number_format($this->amount_to, 2, '.', ''),
+            'amount_fee' => number_format($this->amount_fee, 2, '.', ''),
+        ];
+    }
+}
