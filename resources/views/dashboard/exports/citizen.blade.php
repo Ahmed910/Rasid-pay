c7815a3f74f9f959f0c2b64a670b9818423ae6c6
eman rashad <emanrashad173.fintech@gmail.com>
2022-08-04 12:22:51 +0200

    fix issue in contact pdf

--- resources/views/dashboard/exports/citizen.blade.php
+++ resources/views/dashboard/exports/citizen.blade.php
@@ -1,55 +1,48 @@
-
-
 @extends('dashboard.exports.layout')
 
 @section('content')
+    <table id="departmentTable" class="table">
+        <thead>
+            @include('dashboard.exports.header', ['topic' => 'مستخدمي التطبيق'])
+            <tr>
+                <th>#</th>
+                <th>الاسم</th>
+                <th>رقم الهوية</th>
+                <th>الحالة</th>
+                <th>البطاقة المفعلة</th>
+                <th>تاريخ التسجيل</th>
+                <th>تاريخ انتهاء البطاقة</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($citizens as $citizen)
+                <tr>
+                    <td>{{ $loop->iteration }}</td>
+                    <td>{{ $citizen?->user?->fullname }}</td>
+                    <td>{{ $citizen?->user?->identity_number }}</td>
 
-<table id="departmentTable" class="table">
-  <thead>
-    @include('dashboard.exports.header',['topic'=>'مستخدمي التطبيق'])
-    <tr>
-      <th>#</th>
-      <th>الاسم</th>
-      <th>رقم الهوية</th>
-      <th>الحالة</th>
-      <th>البطاقة المفعلة</th>
-      <th>تاريخ التسجيل</th>
-      <th>تاريخ انتهاء البطاقة</th>
-    </tr>
-  </thead>
-  <tbody>
-    @foreach ($citizens as $citizen)
-    <tr>
-      <td>{{ $loop->iteration }}</td>
-      <td>{{ $citizen?->user?->fullname }}</td>
-      <td>{{ $citizen?->user?->identity_number }}</td>
-      @php
-                $ban_status = match ($citizen?->user?->ban_status) {
-                'active' => trans('dashboard.admin.active_cases.active'),
-                'permanent' => trans('dashboard.admin.active_cases.permanent'),
-                'temporary' => trans('dashboard.admin.active_cases.temporary'),
-                }
-      @endphp
+                    @php
+                        $ban_status = match ($citizen?->user?->ban_status) { 'active' => trans('dashboard.admin.active_cases.active'),  'permanent' => trans('dashboard.admin.active_cases.permanent'),  'temporary' => trans('dashboard.admin.active_cases.temporary'),  'exceeded_attempts' => trans('dashboard.admin.active_cases.exceeded_attempts') };
+                    @endphp
 
-      <td>
-        @if($citizen?->user?->ban_status == 'active')
-                  <div class="active">
-                    <i class="mdi mdi-check-circle-outline"></i>
-                    {{ $ban_status }}
-                  </div>
-                  @else
-                  <div class="unactive">
-                    <i class="mdi mdi-cancel"></i>
-                    {{ $ban_status }}
-                  </div>
-        @endif
-      </td>
-      <td>{{ trans('dashboard.package_types.'. $citizen?->enabledPackage?->package_type) }}</td>
-      <td>{{ $citizen?->enabledPackage?->start_at_dashboard }}</td>
-      <td>{{ $citizen?->enabledPackage?->end_at_dashboard }}</td>
-    </tr>
-    @endforeach
-  </tbody>
-</table>
-
+                    <td>
+                        @if ($citizen?->user?->ban_status == 'active')
+                            <div class="active">
+                                <i class="mdi mdi-check-circle-outline"></i>
+                                {{ $ban_status }}
+                            </div>
+                        @else
+                            <div class="unactive">
+                                <i class="mdi mdi-cancel"></i>
+                                {{ $ban_status }}
+                            </div>
+                        @endif
+                    </td>
+                    <td>{{ trans('dashboard.package_types.' . $citizen?->enabledPackage?->package_type) }}</td>
+                    <td>{{ $citizen?->enabledPackage?->start_at_dashboard }}</td>
+                    <td>{{ $citizen?->enabledPackage?->end_at_dashboard }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 @endsection
