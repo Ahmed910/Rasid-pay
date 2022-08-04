1b8b0699b6095abdb48f1400f4c27eecffe0030f
Khadija Sabry <php-kdija@fintech-sa.com>
2022-08-04 14:10:50 +0200

    contact date

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use App\Models\MessageType\MessageType;
 use App\Traits\Loggable;
 use App\Traits\Uuid;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -44,6 +45,11 @@ class Contact extends Model
         }
 
         $this->attributes['title'] = $value;
+    }
+
+    public function getCreatedAtHumanAttribute($date)
+    {
+        return Carbon::parse($this->attributes['created_at'])->diffForHumans();
     }
     #endregion mutators
 
