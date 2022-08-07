616f09ec4e9835307c474c0843f6afc04eb626c3
Mohamed Fathi <mohamedkdr66@gmail.com>
2022-08-07 13:41:27 +0200

    Fix colspan in export files

--- resources/views/dashboard/exports/header.blade.php
+++ resources/views/dashboard/exports/header.blade.php
@@ -2,7 +2,7 @@
   <th colspan="2" style="padding-right: 1em; padding-left: 1em; text-align: center">
     <img src="{{ public_path('dashboardAssets/images/brand/fintech.png') }}" width="150" style="margin: auto" alt="">
   </th>
-  <th colspan="3">
+  <th colspan="{{ $count ?? 3 }}">
     <h3>تقرير عن {{ $topic }}
     </h3>
     <br>
