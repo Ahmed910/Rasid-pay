34fc82f2980cce94fad2b5af4430ceb03cdb48a1
naderhany96 <naderhany613@gmail.com>
2022-08-07 13:06:41 +0200

    test uploading file

---
+++ app/Models/Test.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Traits\Uuid;
+
+class Test extends Model
+{
+    use HasFactory, Uuid;
+
+    #region properties
+    protected $guarded = [];
+    #endregion properties
+
+    #region mutators
+    #endregion mutators
+
+    #region scopes
+    #endregion scopes
+
+    #region relationships
+    #endregion relationships
+
+    #region custom Methods
+    #endregion custom Methods
+}
