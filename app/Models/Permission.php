ef0372d9cfc953da3b2e216c749ddec72aee6512
tahamohamed2022 <98959213+tahamohamed2022@users.noreply.github.com>
2022-08-08 10:04:37 +0200

    Update Permission.php

--- app/Models/Permission.php
+++ app/Models/Permission.php
@@ -176,8 +176,7 @@ class Permission extends Model
             $permissions->push(self::create(['name' => $name]));
         }
         $permissions->filter(function ($value, $key) {
-            dump($key, $value);
-                        return $key == 'name' && ! str_contains($value,'.edit');
+                        return ! str_contains($value->name,'.edit');
                     })->transform(function ($item) {
                         return self::getTransPermission($item);
                     });
