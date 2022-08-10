c9079eb8800de6e7e2b4277352a210bd33931cf6
Heshamashraf794 <101247632+Heshamashraf794@users.noreply.github.com>
2022-08-10 12:12:02 +0200

    invoice edit design

--- resources/views/dashboard/exports/mobile/invoice/charge.blade.php
+++ resources/views/dashboard/exports/mobile/invoice/charge.blade.php
@@ -1,53 +1,44 @@
 
-<section class="main-pd-wrapper" style="width: 450px; margin: auto">
-  <div style="
-                text-align: center;
-                margin: auto;
-                line-height: 1.5;
-                font-size: 14px;
-                color: #4a4a4a;
-              ">
+<section class="main-pd-wrapper">
+
 
     <img src="{{ public_path($transaction->qr_path) }}" alt="" width="150">
-    <p style="font-weight: bold; color: blue; margin-top: 15px; font-size: 18px;">
+    <p style="font-weight: bold; color: #3f68ba; margin-top: 15px; font-size: 18px;">
       @lang('mobile.invoice.successfully_charged')
     </p>
-  </div>
-  <table style="width: 100%; table-layout: fixed;margin-top:30px;">
+  <table style="width: 100%">
     <thead>
       <tr>
-        <th style="width: 220px;color:blue;">
+        <th style="color:#3f68ba;">
           @lang('mobile.invoice.transaction_type')
         </th>
         <th>{{ trans("dashboard.transaction.type_cases.{$transaction->trans_type}") ?? '' }}</th>
       </tr>
       <tr>
-        <th style="width: 220px;color:blue;">
+        <th style="color:#3f68ba;">
           @lang('mobile.invoice.transaction_date')
         </th>
         <th>{{ $transaction->created_at_date ?? '' }}</th>
       </tr>
 
       <tr>
-        <th style="width: 220px;color:blue;">
+        <th style="color:#3f68ba;">
           @lang('mobile.invoice.reference_number')
         </th>
         <th>{{ $transaction->trans_number ?? '' }}</th>
       </tr>
       <tr>
-        <th style="width: 220px;color:blue;">
+        <th style="color:#3f68ba;">
           @lang('mobile.invoice.charge_amount')
         </th>
         <th>{{ $transaction->amount ?? '' }}ر.س </th>
       </tr>
       <tr>
-        <th style="width: 220px;color:blue;">
+        <th style="color:#3f68ba;">
           @lang('mobile.invoice.phone')
         </th>
         <th>{{ $transaction?->fromUser?->phone ?? '' }} </th>
       </tr>
-
-
     </thead>
     <tbody></tbody>
   </table>
