a7b00c7efaadf0963cd8e6e7e5c6f2af4f166f9d
Heshamashraf794 <101247632+Heshamashraf794@users.noreply.github.com>
2022-08-11 14:05:24 +0200

    Update header.blade.php

--- resources/views/dashboard/exports/header.blade.php
+++ resources/views/dashboard/exports/header.blade.php
@@ -1,13 +1,15 @@
-  <img src="{{ public_path('dashboardAssets/images/brand/fintech.png') }}" width="150" alt="" />
-  <h2>تقرير عن {{ $topic }}</h2>
-  <p>
-    تاريخ إنشائها من ({{ $date_from ?? '' }}) إلى ({{ $date_to ?? '' }})
-  </p>
-  <span style="margin-left: 10px; display: inline-block"><b>رقم المستخدم:</b> {{ $userId ?? auth()->user()->login_id }}</span>
-  &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;
-  <span style="margin-right: 10px; display: inline-block"><b>تاريخ الطباعة:</b> {{ format_date(now()) }}</span>
+<div style="text-align: center; margin-bottom: 1em;">
+  <img src="{{ public_path('dashboardAssets/images/brand/fintech.png') }}" width="150" style="margin: auto; display: block;" alt="">
+  <br>
+  <h2>تقرير عن
+    {{ $topic }}
+  </h2>
+  <p>تاريخ إنشائها من ({{ $date_from ?? '' }}) إلى ({{ $date_to ?? '' }})</p>
+  <span style="margin-left: 10px; display: inline-block;"><b>رقم المستخدم:</b> {{ $userId ?? auth()->user()->login_id }}</span>&nbsp;&nbsp;&nbsp;
+  <span style="margin-right: 10px; display: inline-block;"><b>تاريخ الطباعة:</b> {{ format_date(now()) }}</span>
+  <br>
 
-
+</div>
 <!--
 <tr style="border: none;">
   <th colspan="2" style="padding-right: 1em; padding-left: 1em; text-align: center;border: none;">
