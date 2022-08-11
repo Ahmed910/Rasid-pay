af5ce3632e1d2aea2ed7b2998f10367257caffe9
Heshamashraf794 <101247632+Heshamashraf794@users.noreply.github.com>
2022-08-11 13:22:00 +0200

    Update layout.blade.php

--- resources/views/dashboard/exports/layout.blade.php
+++ resources/views/dashboard/exports/layout.blade.php
@@ -38,9 +38,7 @@
         border-collapse: collapse;
       }
 
-      table th {
-        border: 1px solid #fff !important;
-      }
+
       table td {
         border: 1px solid #c9c9c9 !important;
       }
