a48b24e7aa5055f6169b97f1906902e77b3e2aaa
Heshamashraf794 <101247632+Heshamashraf794@users.noreply.github.com>
2022-08-11 11:43:52 +0200

    edit header design for export table

--- resources/views/dashboard/exports/header.blade.php
+++ resources/views/dashboard/exports/header.blade.php
@@ -1,10 +1,11 @@
-<tr>
-  <th colspan="2" style="padding-right: 1em; padding-left: 1em; text-align: center">
+<tr style="border: none;">
+  <th colspan="2" style="padding-right: 1em; padding-left: 1em; text-align: center;border: none;">
     <img src="{{ public_path('dashboardAssets/images/brand/fintech.png') }}" width="150" style="margin: auto" alt="">
   </th>
-  <th colspan="{{ $count ?? 3 }}">
+  <th colspan="{{ $count ?? 3 }}" style="border: none;">
     <h3 class="text-center" style="margin: 0 auto; position: relative; right: 50%; transform: translateX(-50%)">تقرير عن
-      {{ $topic }} </h3>
+      {{ $topic }}
+    </h3>
     <br>
     <p>تاريخ إنشائها من ({{ $date_from ?? '' }}) إلى ({{ $date_to ?? '' }})</p>
     <br>
