68ff476865f68ddca6d7246b7e8a01bb0571d898
Mohamed Fathi <mohamedkdr66@gmail.com>
2022-08-22 13:55:46 +0200

    Fix issue

--- resources/views/dashboard/exports/contacts.blade.php
+++ resources/views/dashboard/exports/contacts.blade.php
@@ -7,9 +7,10 @@
     <tr>
       <th>#</th>
       <th>@lang('dashboard.contact.user_name')</th>
-      <th>@lang('dashboard.contact.email')</th>
       <th>@lang('dashboard.contact.phone')</th>
-      <th>@lang('dashboard.contact.title')</th>
+      <th>@lang('dashboard.contact.message_type')</th>
+      <th>@lang('dashboard.contact.email')</th>
+      <th>@lang('dashboard.contact.employee_names')</th>
       <th>@lang('dashboard.contact.from_app_or_web')</th>
       <th>@lang('dashboard.contact.status')</th>
     </tr>
@@ -19,9 +20,10 @@
     <tr>
       <td>{{ isset($key) ? $loop->iteration + ($key * $chunk) : $loop->iteration }}</td>
       <td>{{ $contact->fullname }}</td>
-      <td>{{ $contact->email }}</td>
       <td>{{ $contact->phone }}</td>
-      <td>{{ $contact->title }}</td>
+      <td>{{ $contact->messageType?->name }}</td>
+      <td>{{ $contact->email }}</td>
+      <td>{{ $contact->admin->fullname }}</td>
       <td>{{ trans('dashboard.contact.message_sources.'.$contact->message_source) }}</td>
       <td>{{ trans('dashboard.contact.message_status.'.$contact->message_status) }}</td>
     </tr>
