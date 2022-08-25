cde79c2f0be42df8afd059be5df535b29fcb7eb2
Mohamed Fathi <mohamedkdr66@gmail.com>
2022-08-25 11:02:24 +0200

    Fix issue in ourApps

--- resources/views/dashboard/exports/our_app.blade.php
+++ resources/views/dashboard/exports/our_app.blade.php
@@ -7,31 +7,28 @@
     <tr>
       <th>#</th>
       <th>@lang('dashboard.our_app.name')</th>
+      <th>@lang('dashboard.general.created_at')</th>
       <th>@lang('dashboard.our_app.status')</th>
-      <th>@lang('dashboard.our_app.android_link')</th>
-      <th>@lang('dashboard.our_app.ios_link')</th>
     </tr>
   </thead>
   <tbody>
     @foreach ($rows as $our_app)
     <tr>
       <td>{{ isset($key) ? $loop->iteration + ($key * $chunk) : $loop->iteration }}</td>
-      <td>{{ $our_app->name ?? '' }}</td>
+      <td>{{ $our_app->created_at_date ?? '' }}</td>
       <td>
-                @if($our_app->is_active)
-                <div class="active">
-                  <i class="mdi mdi-check-circle-outline"></i>
-                  {{ trans('dashboard.our_app.active_cases.1') }}
-                </div>
-                @else
-                <div class="unactive">
-                  <i class="mdi mdi-cancel"></i>
-                  {{ trans('dashboard.our_app.active_cases.0') }}
-                </div>
-                @endif
+        @if($our_app->is_active)
+        <div class="active">
+          <i class="mdi mdi-check-circle-outline"></i>
+          {{ trans('dashboard.our_app.active_cases.1') }}
+        </div>
+        @else
+        <div class="unactive">
+          <i class="mdi mdi-cancel"></i>
+          {{ trans('dashboard.our_app.active_cases.0') }}
+        </div>
+        @endif
       </td>
-      <td>{{ $our_app->android_link ?? '' }}</td>
-      <td>{{ $our_app->ios_link ?? '' }}</td>
     </tr>
     @endforeach
   </tbody>
